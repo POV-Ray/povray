5b559ecb450fd9b4228fce30fdd9163649fdbf44
Denis Bodor <lefinnois@lefinnois.net>
2015-02-20 16:49:24 +0100

    added map_type 8 for angular mapping aka Debevec mirrorball HDR photo shot, this is type 7 in megapov

--- source/backend/parser/parstxtr.cpp
+++ source/backend/parser/parstxtr.cpp
@@ -580,6 +580,7 @@ void Parser::Parse_Image_Map (PIGMENT *Pigment)
                 case SPHERICAL_MAP:
                 case CYLINDRICAL_MAP:
                 case TORUS_MAP:
+		case ANGULAR_MAP:
                     break;
 
                 default:
