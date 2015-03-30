POV-Ray/povray

commit 29f573ea01855664af90f0b9624ba0feb4ed4bc3

Fixed bug in tuple extensions to #declare and #local causing hard crashes.

source/base/version.h
@@ -45,7 +45,7 @@
 #define OFFICIAL_VERSION_STRING "3.7.1"
 #define OFFICIAL_VERSION_NUMBER 371
 
-#define POV_RAY_PRERELEASE "alpha.7981337"
+#define POV_RAY_PRERELEASE "alpha.8004330"
 
 #if POV_RAY_IS_OFFICIAL == 1
 #ifdef POV_RAY_PRERELEASE

source/parser/parser.cpp
@@ -8215,12 +8215,19 @@ ObjectPtr Parser::Parse_Object_Id ()
 
 void Parser::Parse_Declare(bool is_local, bool after_hash)
 {
-    vector<Token_Struct> lvalues;
+    struct LValue
+    {
+        Token_Struct token;
+        int          previous;
+        SYM_ENTRY*   tempEntry;
+        bool         allowRedefine;
+    };
+    vector<LValue> lvalues;
     bool deprecated = false;
     bool deprecated_once = false;
-    int Previous=-1;
+    int Previous;
     int Local_Index;
-    SYM_ENTRY *Temp_Entry = NULL;
+    SYM_ENTRY *Temp_Entry;
     bool allow_redefine = true;
     UCS2 *deprecation_message;
     bool tupleDeclare = false;
@@ -8272,6 +8279,9 @@ void Parser::Parse_Declare(bool is_local, bool after_hash)
                 UNGET
             END_CASE
         END_EXPECT
+
+        Previous = -1;
+        Temp_Entry = NULL;
 
         EXPECT
             CASE (IDENTIFIER_TOKEN)
@@ -8364,7 +8374,12 @@ void Parser::Parse_Declare(bool is_local, bool after_hash)
             END_CASE
         END_EXPECT
 
-        lvalues.push_back(Token);
+        LValue lvalue;
+        lvalue.token = Token;
+        lvalue.tempEntry = Temp_Entry;
+        lvalue.previous = Previous;
+        lvalue.allowRedefine = allow_redefine;
+        lvalues.push_back(lvalue);
 
         if (lvectorDeclare && lvalues.size() >= 5)
             more = false;
@@ -8408,7 +8423,10 @@ void Parser::Parse_Declare(bool is_local, bool after_hash)
         Promote_Express(expr,&terms,lvalues.size());
         for (int i = 0; i < lvalues.size(); ++i)
         {
-            Token_Struct& t = lvalues[i];
+            Token_Struct& t = lvalues[i].token;
+            Previous = lvalues[i].previous;
+            Temp_Entry = lvalues[i].tempEntry;
+            allow_redefine = lvalues[i].allowRedefine;
 
             *t.NumberPtr = FLOAT_ID_TOKEN;
             Test_Redefine(Previous,t.NumberPtr,*t.DataPtr, allow_redefine);
@@ -8424,7 +8442,10 @@ void Parser::Parse_Declare(bool is_local, bool after_hash)
         }
         for (int i = 0; i < lvalues.size(); ++i)
         {
-            Token_Struct& t = lvalues[i];
+            Token_Struct& t = lvalues[i].token;
+            Previous = lvalues[i].previous;
+            Temp_Entry = lvalues[i].tempEntry;
+            allow_redefine = lvalues[i].allowRedefine;
 
             if (i > 0)
             {

unix/VERSION
@@ -1,1 +1,1 @@
-3.7.1-alpha.7981337
+3.7.1-alpha.8004330

commit dc0fc6d1edeb70117838216e2288d23cfd0fbfad

Fixed Unix build error introduced with commit 29f573e.

source/base/version.h
@@ -45,7 +45,7 @@
 #define OFFICIAL_VERSION_STRING "3.7.1"
 #define OFFICIAL_VERSION_NUMBER 371
 
-#define POV_RAY_PRERELEASE "alpha.8004333"
+#define POV_RAY_PRERELEASE "alpha.8017570"
 
 #if POV_RAY_IS_OFFICIAL == 1
 #ifdef POV_RAY_PRERELEASE

source/parser/parser.cpp
@@ -8215,13 +8215,6 @@ ObjectPtr Parser::Parse_Object_Id ()
 
 void Parser::Parse_Declare(bool is_local, bool after_hash)
 {
-    struct LValue
-    {
-        Token_Struct token;
-        int          previous;
-        SYM_ENTRY*   tempEntry;
-        bool         allowRedefine;
-    };
     vector<LValue> lvalues;
     bool deprecated = false;
     bool deprecated_once = false;

source/parser/parser.h
@@ -242,6 +242,14 @@ class Parser : public SceneTask
             bool is_array_elem;                             ///< true if token is actually an array element reference
         };
 
+        struct LValue
+        {
+            Token_Struct token; // TODO - we probably only need a small subset of the information
+            int          previous;
+            SYM_ENTRY*   tempEntry;
+            bool         allowRedefine;
+        };
+
         Token_Struct Token;
 
         struct Macro

unix/VERSION
@@ -1,1 +1,1 @@
-3.7.1-alpha.8004333
+3.7.1-alpha.8017570