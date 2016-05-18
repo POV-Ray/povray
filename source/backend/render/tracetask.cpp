POV-Ray/povray

commit c89113156da57f9bd4261bfa24342a461e4521a2

Fixed two mesh camera bugs and other camera flaws.
- Fixed FS#254 (mesh_camera type 0 repeats first pixel line).
- Fixed FS#261 (mesh_camera type 3 output image is offset by 0.5 pixels).
- Fixed mesh_camera-related flaws in the parser.
- Fixed a family of bugs that could cause problems when using focal blur with some camera types if the camera is very close to the surface of a media container.
- A bit of refactoring (changed `TracePixel::operator()` semantics).

source/backend/render/tracetask.cpp
@@ -8,7 +8,7 @@
 /// @parblock
 ///
 /// Persistence of Vision Ray Tracer ('POV-Ray') version 3.7.
-/// Copyright 1991-2015 Persistence of Vision Raytracer Pty. Ltd.
+/// Copyright 1991-2016 Persistence of Vision Raytracer Pty. Ltd.
 ///
 /// POV-Ray is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Affero General Public License as
@@ -404,7 +404,7 @@ void TraceTask::SimpleSamplingM0()
                 {
                     TransColour c;
                     gIntersectionTime = 0;
-                    trace(x, y, GetViewData()->GetWidth(), GetViewData()->GetHeight(), c);
+                    trace(x+0.5, y+0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), c);
                     if (gIntersectionTime < it)
                         it = gIntersectionTime;
                 }
@@ -416,7 +416,7 @@ void TraceTask::SimpleSamplingM0()
 #endif
                 RGBTColour col;
 
-                trace(x, y, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
+                trace(x+0.5, y+0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
                 GetViewDataPtr()->Stats()[Number_Of_Pixels]++;
 
                 pixels.push_back(col);
@@ -467,7 +467,7 @@ void TraceTask::SimpleSamplingM0P()
                 {
                     TransColour c;
                     gIntersectionTime = 0;
-                    trace(x, y, GetViewData()->GetWidth(), GetViewData()->GetHeight(), c);
+                    trace(x+0.5, y+0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), c);
                     if (gIntersectionTime < it)
                         it = gIntersectionTime;
                 }
@@ -479,7 +479,7 @@ void TraceTask::SimpleSamplingM0P()
 #endif
                 RGBTColour col;
 
-                trace(x, y, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
+                trace(x+0.5, y+0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
                 GetViewDataPtr()->Stats()[Number_Of_Pixels]++;
 
                 pixelpositions.push_back(Vector2d(x, y));
@@ -515,7 +515,7 @@ void TraceTask::NonAdaptiveSupersamplingM1()
         // sample line above current block
         for(int x = rect.left; x <= rect.right; x++)
         {
-            trace(DBL(x), DBL(rect.top) - 1.0, GetViewData()->GetWidth(), GetViewData()->GetHeight(), pixels(x, rect.top - 1));
+            trace(x+0.5, rect.top-0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), pixels(x, rect.top - 1));
             GetViewDataPtr()->Stats()[Number_Of_Pixels]++;
 
             // Cannot supersample this pixel, so just claim it was already supersampled! [trf]
@@ -527,7 +527,7 @@ void TraceTask::NonAdaptiveSupersamplingM1()
 
         for(int y = rect.top; y <= rect.bottom; y++)
         {
-            trace(DBL(rect.left) - 1.0, y, GetViewData()->GetWidth(), GetViewData()->GetHeight(), pixels(rect.left - 1, y)); // sample pixel left of current line in block
+            trace(rect.left-0.5, y+0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), pixels(rect.left - 1, y)); // sample pixel left of current line in block
             GetViewDataPtr()->Stats()[Number_Of_Pixels]++;
 
             // Cannot supersample this pixel, so just claim it was already supersampled! [trf]
@@ -551,7 +551,7 @@ void TraceTask::NonAdaptiveSupersamplingM1()
             for(int x = rect.left; x <= rect.right; x++)
             {
                 // trace current pixel
-                trace(DBL(x), DBL(y), GetViewData()->GetWidth(), GetViewData()->GetHeight(), pixels(x, y));
+                trace(x+0.5, y+0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), pixels(x, y));
                 GetViewDataPtr()->Stats()[Number_Of_Pixels]++;
 
                 Cooperate();
@@ -602,7 +602,7 @@ void TraceTask::AdaptiveSupersamplingM2()
             for(int x = rect.left; x <= rect.right + 1; x++)
             {
                 // trace upper-left corners of all pixels
-                trace(DBL(x) - 0.5, DBL(y) - 0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), pixels(x, y));
+                trace(x-0.5, y-0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), pixels(x, y));
                 GetViewDataPtr()->Stats()[Number_Of_Pixels]++;
 
                 Cooperate();
@@ -676,10 +676,10 @@ void TraceTask::SupersampleOnePixel(DBL x, DBL y, RGBTColour& col)
             if (jitterScale > 0.0)
             {
                 Jitter2d(x + xx, y + yy, rx, ry);
-                trace(x + xx + (rx * jitterScale), y + yy + (ry * jitterScale), GetViewData()->GetWidth(), GetViewData()->GetHeight(), tempcol);
+                trace(x+0.5 + xx + (rx * jitterScale), y+0.5 + yy + (ry * jitterScale), GetViewData()->GetWidth(), GetViewData()->GetHeight(), tempcol);
             }
             else
-                trace(x + xx, y + yy, GetViewData()->GetWidth(), GetViewData()->GetHeight(), tempcol);
+                trace(x+0.5 + xx, y+0.5 + yy, GetViewData()->GetWidth(), GetViewData()->GetHeight(), tempcol);
 
             col += tempcol;
             GetViewDataPtr()->Stats()[Number_Of_Samples]++;
@@ -740,10 +740,10 @@ void TraceTask::SubdivideOnePixel(DBL x, DBL y, DBL d, size_t bx, size_t by, siz
             if (jitterScale > 0.0)
             {
                 Jitter2d(x - d, y, rxcx0y1, rycx0y1);
-                trace(x - d + (rxcx0y1 * jitterScale), y + (rycx0y1 * jitterScale), GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
+                trace(x+0.5 - d + (rxcx0y1 * jitterScale), y+0.5 + (rycx0y1 * jitterScale), GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
             }
             else
-                trace(x - d, y, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
+                trace(x+0.5 - d, y+0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
 
             buffer.SetSample(bx, by + bstephalf, col);
 
@@ -760,10 +760,10 @@ void TraceTask::SubdivideOnePixel(DBL x, DBL y, DBL d, size_t bx, size_t by, siz
             if (jitterScale > 0.0)
             {
                 Jitter2d(x, y - d, rxcx1y0, rycx1y0);
-                trace(x + (rxcx1y0 * jitterScale), y - d + (rycx1y0 * jitterScale), GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
+                trace(x+0.5 + (rxcx1y0 * jitterScale), y+0.5 - d + (rycx1y0 * jitterScale), GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
             }
             else
-                trace(x, y - d, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
+                trace(x+0.5, y+0.5 - d, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
 
             buffer.SetSample(bx + bstephalf, by, col);
 
@@ -780,10 +780,10 @@ void TraceTask::SubdivideOnePixel(DBL x, DBL y, DBL d, size_t bx, size_t by, siz
             if (jitterScale > 0.0)
             {
                 Jitter2d(x + d, y, rxcx2y1, rycx2y1);
-                trace(x + d + (rxcx2y1 * jitterScale), y + (rycx2y1 * jitterScale), GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
+                trace(x+0.5 + d + (rxcx2y1 * jitterScale), y+0.5 + (rycx2y1 * jitterScale), GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
             }
             else
-                trace(x + d, y, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
+                trace(x+0.5 + d, y+0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
 
             buffer.SetSample(bx + bstep, by + bstephalf, col);
 
@@ -800,10 +800,10 @@ void TraceTask::SubdivideOnePixel(DBL x, DBL y, DBL d, size_t bx, size_t by, siz
             if (jitterScale > 0.0)
             {
                 Jitter2d(x, y + d, rxcx1y2, rycx1y2);
-                trace(x + (rxcx1y2 * jitterScale), y + d + (rycx1y2 * jitterScale), GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
+                trace(x+0.5 + (rxcx1y2 * jitterScale), y+0.5 + d + (rycx1y2 * jitterScale), GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
             }
             else
-                trace(x, y + d, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
+                trace(x+0.5, y+0.5 + d, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
 
             buffer.SetSample(bx + bstephalf, by + bstep, col);
 
@@ -820,10 +820,10 @@ void TraceTask::SubdivideOnePixel(DBL x, DBL y, DBL d, size_t bx, size_t by, siz
             if (jitterScale > 0.0)
             {
                 Jitter2d(x, y, rxcx1y1, rycx1y1);
-                trace(x + (rxcx1y1 * jitterScale), y + (rycx1y1 * jitterScale), GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
+                trace(x+0.5 + (rxcx1y1 * jitterScale), y+0.5 + (rycx1y1 * jitterScale), GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
             }
             else
-                trace(x, y, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
+                trace(x+0.5, y+0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), col);
 
             buffer.SetSample(bx + bstephalf, by + bstephalf, col);
 

source/base/version.h
@@ -45,7 +45,7 @@
 #define OFFICIAL_VERSION_STRING "3.7.1"
 #define OFFICIAL_VERSION_NUMBER 371
 
-#define POV_RAY_PRERELEASE "alpha.8503825"
+#define POV_RAY_PRERELEASE "alpha.8508391"
 
 #if (POV_RAY_IS_AUTOBUILD == 1) && ((POV_RAY_IS_OFFICIAL == 1) || (POV_RAY_IS_SEMI_OFFICIAL == 1))
 #ifdef POV_RAY_PRERELEASE

source/core/render/tracepixel.cpp
@@ -310,10 +310,6 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
     Vector3d V1;
     TRANSFORM Trans;
 
-    // Move to center of pixel
-    x += 0.5;
-    y -= 0.5;
-
     // Set ray flags
     ray.SetFlags(Ray::PrimaryRay, false, false, false, false, pretrace);
 
@@ -328,19 +324,16 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             x0 = x / width - 0.5;
 
             // Convert the y coordinate to be a DBL from -0.5 to 0.5.
-            y0 = ((height - 1) - y) / height - 0.5;
+            y0 = 0.5 - y / height;
 
             // Create primary ray.
             ray.Direction = cameraDirection + x0 * cameraRight + y0 * cameraUp;
 
             // Do focal blurring (by Dan Farmer).
             if(useFocalBlur)
-            {
                 JitterCameraRay(ray, x, y, ray_number);
-                InitRayContainerState(ray, true);
-            }
-            else
-                InitRayContainerState(ray);
+
+            InitRayContainerState(ray, useFocalBlur);
             break;
 
         // Orthographic projection.
@@ -349,7 +342,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             x0 = x / width - 0.5;
 
             // Convert the y coordinate to be a DBL from -0.5 to 0.5.
-            y0 = ((height - 1) - y) / height - 0.5;
+            y0 = 0.5 - y / height;
 
             // Create primary ray.
             ray.Direction = cameraDirection;
@@ -365,10 +358,10 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
         // Fisheye camera.
         case FISHEYE_CAMERA:
             // Convert the x coordinate to be a DBL from -1.0 to 1.0.
-            x0 = 2.0 * x / width - 1.0;
+            x0 = 2.0 * (x / width - 0.5);
 
             // Convert the y coordinate to be a DBL from -1.0 to 1.0.
-            y0 = 2.0 * ((height - 1) - y) / height - 1.0;
+            y0 = 2.0 * (0.5 - y / height);
 
             // This code would do what Warp wants
             x0 *= cameraLengthRight;
@@ -403,16 +396,16 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             if(useFocalBlur)
                 JitterCameraRay(ray, x, y, ray_number);
 
-            InitRayContainerState(ray);
+            InitRayContainerState(ray, useFocalBlur);
             break;
 
         // Omnimax camera.
         case OMNIMAX_CAMERA:
             // Convert the x coordinate to be a DBL from -1.0 to 1.0.
-            x0 = 2.0 * x / width - 1.0;
+            x0 = 2.0 * (x / width - 0.5);
 
             // Convert the y coordinate to be a DBL from -1.0 to 1.0.
-            y0 = 2.0 * ((height - 1) - y) / height - 1.0;
+            y0 = 2.0 * (0.5 - y / height);
 
             // Get polar coordinates.
             if(aspectRatio > 1.0)
@@ -459,7 +452,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             if(useFocalBlur)
                 JitterCameraRay(ray, x, y, ray_number);
 
-            InitRayContainerState(ray);
+            InitRayContainerState(ray, useFocalBlur);
             break;
 
         // Panoramic camera from Graphic Gems III.
@@ -468,7 +461,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             x0 = x / width;
 
             // Convert the y coordinate to be a DBL from -1.0 to 1.0.
-            y0 = 2.0 * ((height - 1) - y) / height - 1.0;
+            y0 = 2.0 * (0.5 - y / height);
 
             // Get cylindrical coordinates.
             x0 = (1.0 - x0) * M_PI;
@@ -493,7 +486,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             if(useFocalBlur)
                 JitterCameraRay(ray, x, y, ray_number);
 
-            InitRayContainerState(ray);
+            InitRayContainerState(ray, useFocalBlur);
             break;
 
         // Ultra wide angle camera written by Dan Farmer.
@@ -502,7 +495,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             x0 = x / width - 0.5;
 
             // Convert the y coordinate to be a DBL from -0.5 to 0.5.
-            y0 = ((height - 1) - y) / height - 0.5;
+            y0 = 0.5 - y / height;
 
             // Create primary ray.
             x0 *= camera.Angle * M_PI_180; // NK 1998 - changed to M_PI_180
@@ -518,7 +511,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             if(useFocalBlur)
                 JitterCameraRay(ray, x, y, ray_number);
 
-            InitRayContainerState(ray);
+            InitRayContainerState(ray, useFocalBlur);
             break;
 
         // Cylinder camera 1. Axis in "up" direction
@@ -527,7 +520,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             x0 = x / width - 0.5;
 
             // Convert the y coordinate to be a DBL from -0.5 to 0.5.
-            y0 = ((height - 1) - y) / height - 0.5;
+            y0 = 0.5 - y / height;
 
             // Create primary ray.
             x0 *= camera.Angle * M_PI_180;
@@ -541,7 +534,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             if(useFocalBlur)
                 JitterCameraRay(ray, x, y, ray_number);
 
-            InitRayContainerState(ray);
+            InitRayContainerState(ray, useFocalBlur);
             break;
 
         // Cylinder camera 2. Axis in "right" direction
@@ -550,7 +543,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             x0 = x / width - 0.5;
 
             // Convert the y coordinate to be a DBL from -0.5 to 0.5.
-            y0 = ((height - 1) - y) / height - 0.5;
+            y0 = 0.5 - y / height;
 
             y0 *= camera.Angle * M_PI_180;
             x0 *= aspectRatio;
@@ -563,7 +556,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             if(useFocalBlur)
                 JitterCameraRay(ray, x, y, ray_number);
 
-            InitRayContainerState(ray);
+            InitRayContainerState(ray, useFocalBlur);
             break;
 
         // Cylinder camera 3. Axis in "up" direction, orthogonal in "right"
@@ -572,7 +565,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             x0 = x / width - 0.5;
 
             // Convert the y coordinate to be a DBL from -0.5 to 0.5.
-            y0 = ((height - 1) - y) / height - 0.5;
+            y0 = 0.5 - y / height;
 
             // Create primary ray.
             x0 *= camera.Angle * M_PI_180;
@@ -597,7 +590,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             x0 = x / width - 0.5;
 
             // Convert the y coordinate to be a DBL from -0.5 to 0.5.
-            y0 = ((height - 1) - y) / height - 0.5;
+            y0 = 0.5 - y / height;
 
             // Create primary ray.
             y0 *= camera.Angle * M_PI_180;
@@ -622,7 +615,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             x0 = x / width - 0.5;
 
             // Convert the y coordinate to be a DBL from -0.5 to 0.5.
-            y0 = ((height - 1) - y) / height - 0.5;
+            y0 = 0.5 - y / height;
 
             // get angle in radians
             y0 *= (camera.V_Angle / 360) * TWO_M_PI;
@@ -641,14 +634,17 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             if(useFocalBlur)
                 JitterCameraRay(ray, x, y, ray_number);
 
-            InitRayContainerState(ray);
+            InitRayContainerState(ray, useFocalBlur);
             break;
 
         case MESH_CAMERA:
             // in the case of the mesh camera, we don't want any pixel co-ordinates that are outside
             // the logical image boundaries (and particularly no negative ones), so we clip them here.
-            x = max(0.0, min(x, width - 1.0));
-            y = max(0.0, min(y, height - 1.0));
+            if (camera.Face_Distribution_Method != 3)
+            {
+                x = max(0.0, min(floor(x), width - 1.0));
+                y = max(0.0, min(floor(y), height - 1.0));
+            }
 
             // Note: while it does not make sense to use AA with distribution methods 0, 1, or 2, we don't prohibit it.
             // The same goes for jitter and a few other non-mesh-camera specific effects. This is primarily because
@@ -693,7 +689,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
                 }
 
                 // we're done
-                InitRayContainerState(ray);
+                InitRayContainerState(ray, true);
             }
             else if (camera.Face_Distribution_Method == 1)
             {
@@ -735,7 +731,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
                             MTransPoint (ray.Origin, ray.Origin, mesh->Trans);
                             MTransNormal (ray.Direction, ray.Direction, mesh->Trans);
                         }
-                        InitRayContainerState(ray);
+                        InitRayContainerState(ray, true);
                         break;
                     }
                 }
@@ -770,7 +766,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
                     MTransPoint (ray.Origin, ray.Origin, mesh->Trans);
                     MTransNormal (ray.Direction, ray.Direction, mesh->Trans);
                 }
-                InitRayContainerState(ray);
+                InitRayContainerState(ray, true);
             }
             else if (camera.Face_Distribution_Method == 3)
             {
@@ -780,8 +776,8 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
                 // presumably have been constructed to scale the pixels evenly across all the faces.
 
                 // convert X and Y into UV co-ordinates
-                double u = (x - 0.5) / width;
-                double v = 1.0 - (y + 0.5) / height;
+                double u = x / width;
+                double v = 1.0 - y / height;
 
                 // now we need to find the first face that that those co-ordinates fall within.
                 // NB while it is of course possible for multiple faces to match a single UV co-ordinate,
@@ -846,7 +842,7 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
                     MTransPoint (ray.Origin, ray.Origin, mesh->Trans);
                     MTransNormal (ray.Direction, ray.Direction, mesh->Trans);
                 }
-                InitRayContainerState(ray);
+                InitRayContainerState(ray, true);
             }
             break;
 
@@ -954,7 +950,7 @@ void TracePixel::TraceRayWithFocalBlur(RGBTColour& colour, DBL x, DBL y, DBL wid
     int i;
     DBL dx, dy, n, randx, randy;
     RGBTColour C, V1, S1, S2;
-    int seed = int(x * 313.0 + 11.0) + int(y * 311.0 + 17.0);
+    int seed = int((x-0.5) * 313.0 + 11.0) + int((y-0.5) * 311.0 + 17.0);
 
     TraceTicket ticket(maxTraceLevel, adcBailout, sceneData->outputAlpha);
     Ray ray(ticket);
@@ -1064,7 +1060,7 @@ void TracePixel::JitterCameraRay(Ray& ray, DBL x, DBL y, size_t ray_number)
 
     deflection = temp_xperp - temp_yperp;
 
-    ray.Origin = camera.Location + deflection;
+    ray.Origin += deflection;
 
     // Deflect the direction of the ray in the opposite direction we deflected
     // the eye position.  This makes sure that we are looking at the same place

source/core/render/tracepixel.h
@@ -68,6 +68,12 @@ class TracePixel : public Trace
         virtual ~TracePixel();
         void SetupCamera(const Camera& cam);
 
+        /// Trace a pixel or sub-pixel.
+        /// @param[in]  x       X-coordinate of the (sub-)pixel's center, typically ranging from 0.5 (left) to width-0.5 (right).
+        /// @param[in]  y       Y-coordinate of the (sub-)pixel's center, typically ranging from 0.5 (top) to height-0.5 (bottom).
+        /// @param[in]  width   Horizontal size of the image in pixels.
+        /// @param[in]  height  Vertical size of the image in pixels.
+        /// @param[out] colour  Computed colour of the (sub-)pixel.
         void operator()(DBL x, DBL y, DBL width, DBL height, RGBTColour& colour);
     private:
         // Focal blur data

source/parser/parser.cpp
@@ -1449,7 +1449,7 @@ void Parser::Parse_Camera (Camera& Cam)
                     END_CASE
 
                     CASE5(ORTHOGRAPHIC_TOKEN, FISHEYE_TOKEN, ULTRA_WIDE_ANGLE_TOKEN, OMNIMAX_TOKEN, PANORAMIC_TOKEN)
-                    CASE2(SPHERICAL_TOKEN, CYLINDER_TOKEN)
+                    CASE3(SPHERICAL_TOKEN, CYLINDER_TOKEN, MESH_CAMERA_TOKEN)
                         Expectation_Error("perspective camera modifier");
                     END_CASE
 
@@ -1469,7 +1469,7 @@ void Parser::Parse_Camera (Camera& Cam)
                     END_CASE
 
                     CASE5(PERSPECTIVE_TOKEN, FISHEYE_TOKEN, ULTRA_WIDE_ANGLE_TOKEN, OMNIMAX_TOKEN, PANORAMIC_TOKEN)
-                    CASE2(SPHERICAL_TOKEN, CYLINDER_TOKEN)
+                    CASE3(SPHERICAL_TOKEN, CYLINDER_TOKEN, MESH_CAMERA_TOKEN)
                         Expectation_Error("orthographic camera modifier");
                     END_CASE
 

unix/VERSION
@@ -1,1 +1,1 @@
-3.7.1-alpha.8503825
+3.7.1-alpha.8508391

commit bf315c986853165228c654479a0d717ee1c41bb1

Fix for AA issue #63 introduced with commit c891131 in March

source/backend/render/tracetask.cpp
@@ -602,7 +602,7 @@ void TraceTask::AdaptiveSupersamplingM2()
             for(int x = rect.left; x <= rect.right + 1; x++)
             {
                 // trace upper-left corners of all pixels
-                trace(x-0.5, y-0.5, GetViewData()->GetWidth(), GetViewData()->GetHeight(), pixels(x, y));
+                trace(x, y, GetViewData()->GetWidth(), GetViewData()->GetHeight(), pixels(x, y));
                 GetViewDataPtr()->Stats()[Number_Of_Pixels]++;
 
                 Cooperate();