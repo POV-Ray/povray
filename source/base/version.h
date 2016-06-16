POV-Ray/povray

commit a10629bf15a7a6938e3300440be55ecb007c0b5a

Internal changes to user-defined function handling.

source/base/version.h
@@ -45,7 +45,7 @@
 #define OFFICIAL_VERSION_STRING "3.7.1"
 #define OFFICIAL_VERSION_NUMBER 371
 
-#define POV_RAY_PRERELEASE "alpha.8566421"
+#define POV_RAY_PRERELEASE "alpha.8579465"
 
 #if (POV_RAY_IS_AUTOBUILD == 1) && ((POV_RAY_IS_OFFICIAL == 1) || (POV_RAY_IS_SEMI_OFFICIAL == 1))
 #ifdef POV_RAY_PRERELEASE

source/core/shape/isosurface.cpp
@@ -103,6 +103,8 @@ bool IsoSurface::All_Intersections(const Ray& ray, IStack& Depth_Stack, TraceThr
     {
         Thread->Stats()[Ray_IsoSurface_Bound_Tests_Succeeded]++;
 
+        GenericScalarFunctionInstance fn(Function, Thread);
+
         in_shadow_test = ray.IsShadowTestRay();
 
         if(Depth1 < 0.0)
@@ -124,7 +126,7 @@ bool IsoSurface::All_Intersections(const Ray& ray, IStack& Depth_Stack, TraceThr
         if(closed != false)
         {
             VTmp = Plocal + Depth1 * Dlocal;
-            tmp = Vector_Function(Thread->functionContext, VTmp);
+            tmp = fn.Evaluate(VTmp);
             if(Depth1 > accuracy)
             {
                 if(tmp < 0.0)                   /* The ray hits the bounding shape */
@@ -145,12 +147,12 @@ bool IsoSurface::All_Intersections(const Ray& ray, IStack& Depth_Stack, TraceThr
                 {
                     Depth1 = accuracy * 5.0;
                     VTmp = Plocal + Depth1 * Dlocal;
-                    if(Vector_Function(Thread->functionContext, VTmp) < 0)
+                    if(fn.Evaluate(VTmp) < 0)
                         Thread->isosurfaceData->Inv3 = -1;
                     /* Change the sign of the function (IPoint is in the bounding shpae.)*/
                 }
                 VTmp = Plocal + Depth2 * Dlocal;
-                if(Vector_Function(Thread->functionContext, VTmp) < 0.0)
+                if(fn.Evaluate(VTmp) < 0.0)
                 {
                     IPoint = ray.Evaluate(Depth2);
                     if(Clip.empty() || Point_In_Clip(IPoint, Clip, Thread))
@@ -176,17 +178,17 @@ bool IsoSurface::All_Intersections(const Ray& ray, IStack& Depth_Stack, TraceThr
         {
             /* IPoint is on the isosurface */
             VTmp = Plocal + tmin * Dlocal;
-            if(fabs(Vector_Function(Thread->functionContext, VTmp)) < (maxg * accuracy * 4.0))
+            if(fabs(fn.Evaluate(VTmp)) < (maxg * accuracy * 4.0))
             {
                 tmin = accuracy * 5.0;
                 VTmp = Plocal + tmin * Dlocal;
-                if(Vector_Function(Thread->functionContext, VTmp) < 0)
+                if(fn.Evaluate(VTmp) < 0)
                     Thread->isosurfaceData->Inv3 = -1;
                 /* change the sign and go into the isosurface */
             }
         }
 
-        Thread->isosurfaceData->ctx = Thread->functionContext;
+        Thread->isosurfaceData->pFn = &fn;
 
         for (; itrace < max_trace; itrace++)
         {
@@ -260,7 +262,7 @@ bool IsoSurface::Inside(const Vector3d& IPoint, TraceThreadData *Thread) const
     if(!container->Inside(New_Point))
         return (Test_Flag(this, INVERTED_FLAG));
 
-    if(Vector_Function(Thread->functionContext, New_Point) > 0)
+    if(GenericScalarFunctionInstance(Function, Thread).Evaluate(New_Point) > 0)
         return (Test_Flag(this, INVERTED_FLAG));
 
     /* Inside the box. */
@@ -307,6 +309,8 @@ void IsoSurface::Normal(Vector3d& Result, Intersection *Inter, TraceThreadData *
     }
     else
     {
+        GenericScalarFunctionInstance fn(Function, Thread);
+
         /* Transform the point into the isosurface space */
         if(Trans != NULL)
             MInvTransPoint(New_Point, Inter->IPoint, Trans);
@@ -314,19 +318,19 @@ void IsoSurface::Normal(Vector3d& Result, Intersection *Inter, TraceThreadData *
             New_Point = Inter->IPoint;
 
         TPoint = New_Point;
-        funct = Evaluate_Function(Function, Thread->functionContext, TPoint);
+        funct = fn.Evaluate(TPoint);
 
         TPoint = New_Point;
         TPoint[X] += accuracy;
-        Result[X] = Evaluate_Function(Function, Thread->functionContext, TPoint) - funct;
+        Result[X] = fn.Evaluate(TPoint) - funct;
             TPoint = New_Point;
 
         TPoint[Y] += accuracy;
-        Result[Y] = Evaluate_Function(Function, Thread->functionContext, TPoint) - funct;
+        Result[Y] = fn.Evaluate(TPoint) - funct;
             TPoint = New_Point;
 
         TPoint[Z] += accuracy;
-        Result[Z] = Evaluate_Function(Function, Thread->functionContext, TPoint) - funct;
+        Result[Z] = fn.Evaluate(TPoint) - funct;
 
         if((Result[X] == 0) && (Result[Y] == 0) && (Result[Z] == 0))
             Result[X] = 1.0;
@@ -930,38 +934,6 @@ bool IsoSurface::Function_Find_Root_R(ISO_ThreadData& itd, const ISO_Pair* EP1, 
 *
 * FUNCTION
 *
-*   Vector_IsoSurface_Function
-*
-* INPUT
-*
-* OUTPUT
-*
-* RETURNS
-*
-* AUTHOR
-*
-*   R. Suzuki
-*
-* DESCRIPTION
-*
-*   -
-*
-* CHANGES
-*
-*   -
-*
-******************************************************************************/
-
-DBL IsoSurface::Vector_Function(GenericFunctionContextPtr ctx, const Vector3d& VPos) const
-{
-    return Evaluate_Function(Function, ctx, VPos) - threshold;
-}
-
-
-/*****************************************************************************
-*
-* FUNCTION
-*
 *   Float_IsoSurface_Function
 *
 * INPUT
@@ -990,42 +962,7 @@ DBL IsoSurface::Float_Function(ISO_ThreadData& itd, DBL t) const
 
     VTmp = itd.Pglobal + t * itd.Dglobal;
 
-    return ((DBL)itd.Inv3 * (Evaluate_Function(Function, itd.ctx, VTmp) - threshold));
-}
-
-
-/*****************************************************************************
- *
- * FUNCTION
- *
- *   Evaluate_Function
- *
- * INPUT
- *
- * OUTPUT
- *
- * RETURNS
- *
- * AUTHOR
- *
- * DESCRIPTION
- *
- *   -
- *
- * CHANGES
- *
- *   -
- *
- ******************************************************************************/
-
-DBL IsoSurface::Evaluate_Function(GenericScalarFunctionPtr funct, GenericFunctionContextPtr ctx, const Vector3d& fnvec)
-{
-    funct->InitArguments(ctx);
-    funct->PushArgument(ctx, fnvec[X]);
-    funct->PushArgument(ctx, fnvec[Y]);
-    funct->PushArgument(ctx, fnvec[Z]);
-
-    return funct->Execute(ctx);
+    return ((DBL)itd.Inv3 * (itd.pFn->Evaluate(VTmp) - threshold));
 }
 
 }

unix/VERSION
@@ -1,1 +1,1 @@
-3.7.1-alpha.8566421
+3.7.1-alpha.8579465

source/core/coretypes.h
@@ -558,45 +558,98 @@ struct FunctionSourceInfo
 template<typename RETURN_T, typename ARG_T>
 class GenericCustomFunction
 {
-    public:
-        virtual ~GenericCustomFunction() {}
-        virtual GenericFunctionContextPtr NewContext(TraceThreadData* pThreadData) = 0;
-        virtual void InitArguments(GenericFunctionContextPtr pContext) = 0;
-        virtual void PushArgument(GenericFunctionContextPtr pContext, ARG_T arg) = 0;
-        virtual RETURN_T Execute(GenericFunctionContextPtr pContext) = 0;
-        virtual GenericCustomFunction* Clone() const = 0;
-        virtual const FunctionSourceInfo* GetSourceInfo() const { return NULL; }
-
-        template<typename T1>
-        inline RETURN_T Execute(GenericFunctionContextPtr pContext, T1 arg1)
-        {
-            InitArguments(pContext);
-            PushArgument(pContext, arg1);
-            return Execute(pContext);
-        }
-
-        template<typename T1, typename T2>
-        inline RETURN_T Execute(GenericFunctionContextPtr pContext, T1 arg1, T2 arg2)
-        {
-            InitArguments(pContext);
-            PushArgument(pContext, arg1);
-            PushArgument(pContext, arg2);
-            return Execute(pContext);
-        }
-
-        template<typename T1, typename T2, typename T3>
-        inline RETURN_T Execute(GenericFunctionContextPtr pContext, T1 arg1, T2 arg2, T3 arg3)
-        {
-            InitArguments(pContext);
-            PushArgument(pContext, arg1);
-            PushArgument(pContext, arg2);
-            PushArgument(pContext, arg3);
-            return Execute(pContext);
-        }
+public:
+    virtual ~GenericCustomFunction() {}
+    virtual GenericFunctionContextPtr AcquireContext(TraceThreadData* pThreadData) = 0;
+    virtual void ReleaseContext(GenericFunctionContextPtr pContext) = 0;
+    virtual void InitArguments(GenericFunctionContextPtr pContext) = 0;
+    virtual void PushArgument(GenericFunctionContextPtr pContext, ARG_T arg) = 0;
+    virtual RETURN_T Execute(GenericFunctionContextPtr pContext) = 0;
+    virtual GenericCustomFunction* Clone() const = 0;
+    virtual const FunctionSourceInfo* GetSourceInfo() const { return NULL; }
 };
 
 typedef GenericCustomFunction<double, double> GenericScalarFunction;
 typedef GenericScalarFunction* GenericScalarFunctionPtr;
+
+template<typename RETURN_T, typename ARG_T>
+class GenericCustomFunctionInstance
+{
+public:
+    inline GenericCustomFunctionInstance(GenericCustomFunction<RETURN_T,ARG_T>* pFn, TraceThreadData* pThreadData) :
+        mpFunction(pFn), mpContext(pFn->AcquireContext(pThreadData)), mReInit(true)
+    {
+        POV_ASSERT(mpFunction != NULL);
+        POV_ASSERT(mpContext  != NULL);
+    }
+
+    inline ~GenericCustomFunctionInstance()
+    {
+        mpFunction->ReleaseContext(mpContext);
+    }
+
+    inline void PushArgument(ARG_T arg)
+    {
+        if (mReInit)
+        {
+            mpFunction->InitArguments(mpContext);
+            mReInit = false;
+        }
+        mpFunction->PushArgument(mpContext, arg);
+    }
+
+    inline RETURN_T Evaluate()
+    {
+        RETURN_T result = mpFunction->Execute(mpContext);
+        mReInit = true;
+        return result;
+    }
+
+    template<typename T1>
+    inline RETURN_T Evaluate(T1 arg1)
+    {
+        PushArgument(arg1);
+        return Evaluate();
+    }
+
+    template<typename T1, typename T2>
+    inline RETURN_T Evaluate(T1 arg1, T2 arg2)
+    {
+        PushArgument(arg1);
+        PushArgument(arg2);
+        return Evaluate();
+    }
+
+    template<typename T1, typename T2, typename T3>
+    inline RETURN_T Evaluate(T1 arg1, T2 arg2, T3 arg3)
+    {
+        PushArgument(arg1);
+        PushArgument(arg2);
+        PushArgument(arg3);
+        return Evaluate();
+    }
+
+    inline RETURN_T Evaluate(const Vector2d& argV)
+    {
+        return Evaluate(argV.u(), argV.v());
+    }
+
+    inline RETURN_T Evaluate(const Vector3d& argV)
+    {
+        return Evaluate(argV.x(), argV.y(), argV.z());
+    }
+
+protected:
+    GenericCustomFunction<RETURN_T,ARG_T>*  mpFunction;
+    GenericFunctionContextPtr               mpContext;
+    bool                                    mReInit;
+
+private:
+    GenericCustomFunctionInstance();
+};
+
+typedef GenericCustomFunctionInstance<double, double> GenericScalarFunctionInstance;
+typedef GenericScalarFunctionInstance* GenericScalarFunctionInstancePtr;
 
 }
 

source/core/material/pattern.cpp
@@ -378,12 +378,12 @@ DBL FacetsPattern::EvaluateRaw(const Vector3d& EPoint, const Intersection *pIsec
 
 
 FunctionPattern::FunctionPattern() :
-    pFn(NULL), contextId(0)
+    pFn(NULL)
 {}
 
 FunctionPattern::FunctionPattern(const FunctionPattern& obj) :
     ContinuousPattern(obj),
-    pFn(obj.pFn->Clone()), contextId(obj.contextId)
+    pFn(obj.pFn->Clone())
 {}
 
 FunctionPattern::~FunctionPattern()
@@ -6113,20 +6113,7 @@ DBL DentsPattern::EvaluateRaw(const Vector3d& EPoint, const Intersection *pIsect
 
 DBL FunctionPattern::EvaluateRaw(const Vector3d& EPoint, const Intersection *pIsection, const Ray *pRay, TraceThreadData *pThread) const
 {
-    DBL value;
-
-    if(pThread->functionPatternContext[contextId] == NULL)
-        pThread->functionPatternContext[contextId] = pFn->NewContext(const_cast<TraceThreadData *>(pThread));
-
-    GenericFunctionContextPtr ctx = pThread->functionPatternContext[contextId];
-
-    pFn->InitArguments(ctx);
-    pFn->PushArgument(ctx, EPoint[X]);
-    pFn->PushArgument(ctx, EPoint[Y]);
-    pFn->PushArgument(ctx, EPoint[Z]);
-
-    value = pFn->Execute(ctx);
-
+    DBL value = GenericScalarFunctionInstance(pFn, pThread).Evaluate(EPoint);
     return ((value > 1.0) ? fmod(value, 1.0) : value);
 }
 

source/core/material/pattern.h
@@ -555,9 +555,6 @@ struct FunctionPattern : public ContinuousPattern
 {
     GenericScalarFunctionPtr pFn;
 
-    /// The index into the functionPatternContext list, identifying the context to use when calling the function.
-    unsigned int contextId;
-
     FunctionPattern();
     FunctionPattern(const FunctionPattern& obj);
     virtual ~FunctionPattern();

source/core/render/tracepixel.cpp
@@ -200,9 +200,13 @@ TracePixel::TracePixel(shared_ptr<SceneData> sd, const Camera* cam, TraceThreadD
                        focalBlurData(NULL),
                        maxTraceLevel(mtl),
                        adcBailout(adcb),
-                       pretrace(pt),
-                       mpFunctionContext(NULL)
+                       pretrace(pt)
 {
+    for (unsigned int i = 0; i < 3; ++i)
+    {
+        mpCameraLocationFn[i] = NULL;
+        mpCameraDirectionFn[i] = NULL;
+    }
     SetupCamera((cam == NULL) ? sd->parsedCamera : *cam);
 }
 
@@ -210,8 +214,13 @@ TracePixel::~TracePixel()
 {
     if(focalBlurData != NULL)
         delete focalBlurData;
-    if (mpFunctionContext != NULL)
-        delete mpFunctionContext;
+    for (unsigned int i = 0; i < 3; ++i)
+    {
+        if (mpCameraLocationFn[i] != NULL)
+            delete mpCameraLocationFn[i];
+        if (mpCameraDirectionFn[i] != NULL)
+            delete mpCameraDirectionFn[i];
+    }
 }
 
 void TracePixel::SetupCamera(const Camera& cam)
@@ -251,7 +260,17 @@ void TracePixel::SetupCamera(const Camera& cam)
             break;
         case USER_DEFINED_CAMERA:
             normalise = true;
-            mpFunctionContext = sceneData->functionContextFactory->CreateFunctionContext(threadData);
+            for (unsigned int i = 0; i < 3; ++i)
+            {
+                if(mpCameraLocationFn[i] != NULL)
+                    delete mpCameraLocationFn[i];
+                if(camera.Location_Fn[i] != NULL)
+                    mpCameraLocationFn[i] = new GenericScalarFunctionInstance(camera.Location_Fn[i], threadData);
+                if(mpCameraDirectionFn[i] != NULL)
+                    delete mpCameraDirectionFn[i];
+                if(camera.Direction_Fn[i] != NULL)
+                    mpCameraDirectionFn[i] = new GenericScalarFunctionInstance(camera.Direction_Fn[i], threadData);
+            }
             break;
         default:
             aspectRatio = cameraLengthRight / cameraLengthUp;
@@ -863,9 +882,9 @@ bool TracePixel::CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, 
             for (unsigned int i = 0; i < 3; ++i)
             {
                 if (camera.Location_Fn[i] != NULL)
-                    cameraLocation[i] = camera.Location_Fn[i]->Execute(mpFunctionContext, x0, y0);
+                    cameraLocation[i] = mpCameraLocationFn[i]->Evaluate(x0, y0);
                 if (camera.Direction_Fn[i] != NULL)
-                    cameraDirection[i] = camera.Direction_Fn[i]->Execute(mpFunctionContext, x0, y0);
+                    cameraDirection[i] = mpCameraDirectionFn[i]->Evaluate(x0, y0);
             }
             if (cameraDirection.IsNearNull(EPSILON))
                 return false;

source/core/render/tracepixel.h
@@ -128,8 +128,9 @@ class TracePixel : public Trace
         /// whether this is just a pretrace, allowing some computations to be skipped
         bool pretrace;
 
-        /// Function execution context for user-defined camera
-        GenericFunctionContextPtr mpFunctionContext;
+        /// Thread-local instances of user-defined camera functions
+        GenericScalarFunctionInstancePtr mpCameraLocationFn[3];
+        GenericScalarFunctionInstancePtr mpCameraDirectionFn[3];
 
         bool CreateCameraRay(Ray& ray, DBL x, DBL y, DBL width, DBL height, size_t ray_number);
 

commit 088927594868bb6af28ee2e7f4789e5a516b4683

Fix isosurface bug introduced with commit a10629b.

source/base/version.h
@@ -45,7 +45,7 @@
 #define OFFICIAL_VERSION_STRING "3.7.1"
 #define OFFICIAL_VERSION_NUMBER 371
 
-#define POV_RAY_PRERELEASE "alpha.8649449"
+#define POV_RAY_PRERELEASE "alpha.8656843"
 
 #if (POV_RAY_IS_AUTOBUILD == 1) && ((POV_RAY_IS_OFFICIAL == 1) || (POV_RAY_IS_SEMI_OFFICIAL == 1))
 #ifdef POV_RAY_PRERELEASE

source/core/shape/isosurface.cpp
@@ -126,7 +126,7 @@ bool IsoSurface::All_Intersections(const Ray& ray, IStack& Depth_Stack, TraceThr
         if(closed != false)
         {
             VTmp = Plocal + Depth1 * Dlocal;
-            tmp = fn.Evaluate(VTmp);
+            tmp = fn.Evaluate(VTmp) - threshold;
             if(Depth1 > accuracy)
             {
                 if(tmp < 0.0)                   /* The ray hits the bounding shape */
@@ -147,12 +147,12 @@ bool IsoSurface::All_Intersections(const Ray& ray, IStack& Depth_Stack, TraceThr
                 {
                     Depth1 = accuracy * 5.0;
                     VTmp = Plocal + Depth1 * Dlocal;
-                    if(fn.Evaluate(VTmp) < 0)
+                    if(fn.Evaluate(VTmp) < threshold)
                         Thread->isosurfaceData->Inv3 = -1;
                     /* Change the sign of the function (IPoint is in the bounding shpae.)*/
                 }
                 VTmp = Plocal + Depth2 * Dlocal;
-                if(fn.Evaluate(VTmp) < 0.0)
+                if(fn.Evaluate(VTmp) < threshold)
                 {
                     IPoint = ray.Evaluate(Depth2);
                     if(Clip.empty() || Point_In_Clip(IPoint, Clip, Thread))
@@ -178,11 +178,11 @@ bool IsoSurface::All_Intersections(const Ray& ray, IStack& Depth_Stack, TraceThr
         {
             /* IPoint is on the isosurface */
             VTmp = Plocal + tmin * Dlocal;
-            if(fabs(fn.Evaluate(VTmp)) < (maxg * accuracy * 4.0))
+            if(fabs(fn.Evaluate(VTmp) - threshold) < (maxg * accuracy * 4.0))
             {
                 tmin = accuracy * 5.0;
                 VTmp = Plocal + tmin * Dlocal;
-                if(fn.Evaluate(VTmp) < 0)
+                if(fn.Evaluate(VTmp) < threshold)
                     Thread->isosurfaceData->Inv3 = -1;
                 /* change the sign and go into the isosurface */
             }
@@ -262,7 +262,7 @@ bool IsoSurface::Inside(const Vector3d& IPoint, TraceThreadData *Thread) const
     if(!container->Inside(New_Point))
         return (Test_Flag(this, INVERTED_FLAG));
 
-    if(GenericScalarFunctionInstance(Function, Thread).Evaluate(New_Point) > 0)
+    if(GenericScalarFunctionInstance(Function, Thread).Evaluate(New_Point) > threshold)
         return (Test_Flag(this, INVERTED_FLAG));
 
     /* Inside the box. */

unix/VERSION
@@ -1,1 +1,1 @@
-3.7.1-alpha.8649449
+3.7.1-alpha.8656843