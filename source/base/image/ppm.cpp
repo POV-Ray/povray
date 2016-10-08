POV-Ray/povray

commit be34de431090d339b8c407a643571d57d709f39d

Major overhaul of Netpbm (PGM/PPM) input, fixing GitHub issue #111.

source/base/image/ppm.cpp
@@ -2,20 +2,19 @@
 ///
 /// @file base/image/ppm.cpp
 ///
-/// Implementation of NetPBM Portable Pixmap/Graymap (PPM, PGM) image file
+/// Implementation of Netpbm Portable Pixmap/Graymap (PPM/PGM) image file
 /// handling.
 ///
-/// This module contains the code to read and write the PPM file format,
-/// as well as write the PGM file format, according to NetPBM specs
-/// (http://netpbm.sourceforge.net/doc/):
+/// This module contains the code to read and write the PPM and PGM file formats
+/// according to Netpbm specs (http://netpbm.sourceforge.net/doc/):
 ///
-/// For reading both ASCII and binary files are supported ('P3' and 'P6').
+/// For input, both ASCII ("plain") and binary ("raw") formats (magic numbers
+/// `P2`/`P3` and `P5`/`P6`, respectively), are supported.
 ///
-/// For writing we use binary files. OutputQuality > 8 leads to 16 bit files.
-/// Special handling of Greyscale_Output=on -> 16 bit PGM files ('P5').
-///
-/// @note
-///     PGM reading is implemented in @ref pgm.cpp.
+/// For outout we write binary ("raw") PPM files (magic number `P6`), unless
+/// `Greyscale_Output=on` is specified in which case we write binary PGM files
+/// (magic number `P5`). Maxvalue is set to 255 if a bit depth of 8 or lower
+/// is chosen, or 65535 otherwise.
 ///
 /// @copyright
 /// @parblock
@@ -58,7 +57,6 @@
 // POV-Ray base header files
 #include "base/types.h"
 #include "base/image/metadata.h"
-#include "base/image/pgm.h"
 
 // this must be the last file included
 #include "base/povdebug.h"
@@ -66,8 +64,17 @@
 namespace pov_base
 {
 
-namespace Ppm
+namespace Netpbm
 {
+
+enum NetpbmDataFormat
+{
+    kNetpbmDataFormat_ASCII,
+    kNetpbmDataFormat_8bit,
+    kNetpbmDataFormat_16bit,
+};
+
+/*****************************************************************************/
 
 void Write (OStream *file, const Image *image, const Image::WriteOptions& options)
 {
@@ -196,188 +203,197 @@ void Write (OStream *file, const Image *image, const Image::WriteOptions& option
     }
 }
 
-/*****************************************************************************
-*
-* FUNCTION
-*
-*  Read_PPM_Image
-*
-* INPUT
-*
-* OUTPUT
-*
-* RETURNS
-*
-* AUTHOR
-*
-*    Christoph Hormann
-*
-* DESCRIPTION
-*
-*    Reads an PPM image file
-*
-* CHANGES
-*
-*    August 2003 - New implementation based on targa/png reading code
-*
-******************************************************************************/
+/*****************************************************************************/
+
+/// Read an individual character from a Netpbm file, potentially skipping comments.
+inline static int ReadNetpbmAsciiChar (IStream *file, bool allowComments)
+{
+    int c = file->Read_Byte();
+
+    if (allowComments && (c == '#'))
+    {
+        do
+        {
+            c = file->Read_Byte();
+        }
+        while ((c != '\n') && (c != EOF));
+    }
+
+    return c;
+}
+
+/// Read an plain ASCII numeric value from a Netpbm file, potentially skipping comments.
+static POV_UINT32 ReadNetpbmAsciiValue (IStream *file, bool allowComments)
+{
+    POV_UINT32 value = 0;
+    int c;
+    int pos = 0;
+    char buffer[50] = "";
+
+    do
+    {
+        c = ReadNetpbmAsciiChar (file, allowComments);
+        // TODO - we may want to warn in case we just encountered a CR.
+    }
+    while (isspace (c));
+
+    if (!isdigit (c))
+        throw POV_EXCEPTION(kFileDataErr, "Invalid data in Netpbm (PGM/PPM) file");
+
+    do
+    {
+        POV_UINT32 oldValue = value;
+        value = value * 10 + (c-'0');
+        if (value < oldValue)
+            // numeric overflow occurred
+            throw POV_EXCEPTION(kFileDataErr, "Excessively large value in Netpbm (PGM/PPM) file");
+        c = ReadNetpbmAsciiChar (file, allowComments);
+    }
+    while (isdigit(c));
+
+    if ((c != EOF) && (!isspace (c)))
+        throw POV_EXCEPTION(kFileDataErr, "Invalid data in Netpbm (PGM/PPM) file");
+
+    // TODO - we may want to warn in case we just encountered a CR.
+
+    return value;
+}
+
+/// Read an individual raster value from a Netpbm file.
+inline static POV_UINT16 ReadNetpbmRasterValue (IStream *file, NetpbmDataFormat format)
+{
+    if (format == kNetpbmDataFormat_ASCII)
+        return ReadNetpbmAsciiValue (file, false);
+
+    int hi, lo;
+
+    if (format == kNetpbmDataFormat_16bit)
+    {
+        hi = file->Read_Byte();
+        if (hi == EOF)
+            throw POV_EXCEPTION(kFileDataErr, "Unexpected end of file in Netpbm (PGM/PPM) file");
+    }
+    else
+        hi = 0;
+
+    lo = file->Read_Byte();
+    if (lo == EOF)
+        throw POV_EXCEPTION(kFileDataErr, "Unexpected end of file in Netpbm (PGM/PPM) file");
+
+    return (POV_UINT16(hi) << 8) + POV_UINT16(lo);
+}
+
+/*****************************************************************************/
 
 // TODO: make sure we destroy the image if we throw an exception
 Image *Read (IStream *file, const Image::ReadOptions& options)
 {
-    int                   width;
-    int                   height;
-    int                   data_hi; // not unsigned as it may need to hold EOF
-    int                   data_lo; // not unsigned as it may need to hold EOF
-    char                  line[1024];
-    char                  *ptr;
-    Image                 *image = NULL;
-    unsigned int          depth;
-    int                   r; // not unsigned as it may need to hold EOF
-    int                   g; // not unsigned as it may need to hold EOF
-    int                   b; // not unsigned as it may need to hold EOF
-    unsigned char         header[2];
+    POV_UINT32          width;
+    POV_UINT32          height;
+    POV_UINT32          maxval;
+    Image               *image = NULL;
+    unsigned char       magicNumber[2];
 
-    // PPM files may or may not be gamma-encoded.
+    bool                isMonochrome;
+    bool                isAsciiData;
+    bool                is16BitData;
+    NetpbmDataFormat    dataFormat;
+    POV_UINT16          maxImageVal;
+    POV_UINT16          r,g,b;
+
+    // PGM files may or may not be gamma-encoded.
     GammaCurvePtr gamma;
     if (options.gammacorrect && options.defaultGamma)
         gamma = TranscodingGammaCurve::Get(options.workingGamma, options.defaultGamma);
 
     // --- Read Header ---
-    if (!file->read(header, 2))
-        throw POV_EXCEPTION(kFileDataErr, "Cannot read header of PPM image");
+    if (!file->read(magicNumber, 2))
+        throw POV_EXCEPTION(kFileDataErr, "Failed to read Netpbm (PGM/PPM) magic number");
 
-    if(header[0] != 'P')
-        throw POV_EXCEPTION(kFileDataErr, "File is not in PPM format");
+    if (magicNumber[0] != 'P')
+        throw POV_EXCEPTION(kFileDataErr, "File is not a supported Netpbm (PGM/PPM) file");
 
-    if ((header[1] != '3') && (header[1] != '6'))
-        throw POV_EXCEPTION(kFileDataErr, "File is not in PPM format");
-
-    // TODO FIXME - Some valid PPM files may have a different header layout regarding line breaks
-
-    do
+    switch (magicNumber[1])
     {
-        file->getline (line, 1024);
-        line[1023] = '\0';
-        if ((ptr = strchr(line, '#')) != NULL)
-            *ptr = '\0';  // remove comment
-    } while (line[0]=='\0');  // read until line without comment from beginning
+        case '2': // "plain" (ASCII) Portable Gray Map (PGM)
+            isMonochrome = true;
+            isAsciiData  = true;
+            break;
 
-    // --- First: two numbers: width and height ---
-    if (sscanf(line,"%d %d",&width, &height) != 2)
-        throw POV_EXCEPTION(kFileDataErr, "Cannot read width and height from PPM image");
+        case '3': // "plain" (ASCII) Portable Pixel Map (PPM)
+            isMonochrome = false;
+            isAsciiData  = true;
+            break;
 
-    if (width <= 0 || height <= 0)
-        throw POV_EXCEPTION(kFileDataErr, "Invalid width or height read from PPM image");
+        case '5': // "raw" (binary) Portable Gray Map (PGM)
+            isMonochrome = true;
+            isAsciiData  = false;
+            break;
 
-    do
-    {
-        file->getline (line, 1024) ;
-        line[1023] = '\0';
-        if ((ptr = strchr(line, '#')) != NULL)
-            *ptr = '\0';  // remove comment
-    } while (line[0]=='\0');  // read until line without comment from beginning
+        case '6': // "raw" (binary) Portable Pixel Map (PPM)
+            isMonochrome = false;
+            isAsciiData  = false;
+            break;
 
-    // --- Second: one number: color depth ---
-    if (sscanf(line,"%u",&depth) != 1)
-        throw POV_EXCEPTION(kFileDataErr, "Cannot read color depth from PPM image");
-
-    if ((depth > 65535) || (depth < 1))
-        throw POV_EXCEPTION(kFileDataErr, "Unsupported number of colors in PPM image");
-
-    if (depth < 256)
-    {
-        // We'll be using an image container that provides for automatic decoding if possible - unless there's no such decoding to do.
-        gamma = ScaledGammaCurve::GetByDecoding(255.0f/depth, gamma); // Note that we'll apply the scaling even if we don't officially gamma-correct
-        Image::ImageDataType imagetype = options.itype;
-        if (imagetype == Image::Undefined)
-            imagetype = ( GammaCurve::IsNeutral(gamma) ? Image::RGB_Int8 : Image::RGB_Gamma8);
-        image = Image::Create (width, height, imagetype) ;
-        // NB: PPM files don't use alpha, so premultiplied vs. non-premultiplied is not an issue
-        image->TryDeferDecoding(gamma, 255); // try to have gamma adjustment being deferred until image evaluation.
-
-        for (int i = 0; i < height; i++)
-        {
-            if (header[1] == '3') // --- ASCII PPM file (type 3) ---
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    r = Pgm::Read_ASCII_File_Number(file);
-                    g = Pgm::Read_ASCII_File_Number(file);
-                    b = Pgm::Read_ASCII_File_Number(file);
-                    SetEncodedRGBValue (image, x, i, gamma, 255, r, g, b) ;
-                }
-            }
-            else                  // --- binary PPM file (type 6) ---
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    if ((r = file->Read_Byte ()) == EOF)
-                        throw POV_EXCEPTION(kFileDataErr, "Unexpected EOF in PPM file");
-                    if ((g = file->Read_Byte ()) == EOF)
-                        throw POV_EXCEPTION(kFileDataErr, "Unexpected EOF in PPM file");
-                    if ((b = file->Read_Byte ()) == EOF)
-                        throw POV_EXCEPTION(kFileDataErr, "Unexpected EOF in PPM file");
-                    SetEncodedRGBValue (image, x, i, gamma, 255, r, g, b) ;
-                }
-            }
-        }
+        case '1': // "plain" (ASCII) Portable Bit Map (PBM)
+        case '4': // "raw" (binary) Portable Bit Map (PBM)
+        case '7': // Portable Arbitrary Map (PAM)
+        default:
+            throw POV_EXCEPTION(kFileDataErr, "File is not a supported Netbpm (PGM/PPM) file");
+            break;
     }
-    else // --- 16 bit PPM (binary or ASCII) ---
+
+    width  = ReadNetpbmAsciiValue (file, true);
+    height = ReadNetpbmAsciiValue (file, true);
+    maxval = ReadNetpbmAsciiValue (file, true);
+
+    if ((maxval > 65535) || (maxval < 1))
+        throw POV_EXCEPTION(kFileDataErr, "Unsupported number of brightness levels in Netpbm (PGM/PPM) file");
+
+    is16BitData = (maxval > 255);
+
+    dataFormat  = (isAsciiData ? kNetpbmDataFormat_ASCII : (is16BitData ? kNetpbmDataFormat_16bit : kNetpbmDataFormat_8bit));
+    maxImageVal = (is16BitData ? 65535 : 255);
+
+    // We'll be using an image container that provides for automatic decoding if possible - unless there's no such decoding to do.
+    gamma = ScaledGammaCurve::GetByDecoding(float(maxImageVal)/float(maxval), gamma); // Note that we'll apply the scaling even if we don't officially gamma-correct
+    Image::ImageDataType imagetype = options.itype;
+    if (imagetype == Image::Undefined)
+        imagetype = Image::GetImageDataType( GammaCurve::IsNeutral(gamma) ? (is16BitData ? Image::kImageChannelDataType_Int16
+                                                                                         : Image::kImageChannelDataType_Int8)
+                                                                          : (is16BitData ? Image::kImageChannelDataType_Gamma16
+                                                                                         : Image::kImageChannelDataType_Gamma8),
+                                             isMonochrome ? Image::kImageChannelLayout_Gray
+                                                          : Image::kImageChannelLayout_RGB );
+    image = Image::Create (width, height, imagetype) ;
+    // NB: PGM files don't use alpha, so premultiplied vs. non-premultiplied is not an issue
+    image->TryDeferDecoding(gamma, maxImageVal); // try to have gamma adjustment being deferred until image evaluation.
+
+    for (int i = 0; i < height; i++)
     {
-        // We'll be using an image container that provides for automatic decoding if possible - unless there's no such decoding to do.
-        gamma = ScaledGammaCurve::GetByDecoding(65535.0f/depth, gamma); // Note that we'll apply the scaling even if we don't officially gamma-correct
-        Image::ImageDataType imagetype = options.itype;
-        if (imagetype == Image::Undefined)
-            imagetype = ( GammaCurve::IsNeutral(gamma) ? Image::RGB_Int16 : Image::RGB_Gamma16);
-        image = Image::Create (width, height, imagetype) ;
-        // NB: PPM files don't use alpha, so premultiplied vs. non-premultiplied is not an issue
-        image->TryDeferDecoding(gamma, 65535); // try to have gamma adjustment being deferred until image evaluation.
-
-        for (int i = 0; i < height; i++)
+        for (int x = 0; x < width; x++)
         {
-            if (header[1] == '3') // --- ASCII PPM file (type 3) ---
+            if (isMonochrome)
             {
-                for (int x = 0; x < width; x++)
-                {
-                    r = Pgm::Read_ASCII_File_Number(file);
-                    g = Pgm::Read_ASCII_File_Number(file);
-                    b = Pgm::Read_ASCII_File_Number(file);
-                    SetEncodedRGBValue (image, x, i, gamma, 65535, r, g, b) ;
-                }
+                g = ReadNetpbmRasterValue (file, dataFormat);
+                SetEncodedGrayValue (image, x, i, gamma, maxImageVal, g);
             }
-            else                  // --- binary PPM file (type 6) ---
+            else
             {
-                for (int x = 0; x < width; x++)
-                {
-                    if ((data_hi = file->Read_Byte ()) == EOF)
-                        throw POV_EXCEPTION(kFileDataErr, "Unexpected EOF in PPM file");
-                    if ((data_lo = file->Read_Byte ()) == EOF)
-                        throw POV_EXCEPTION(kFileDataErr, "Unexpected EOF in PPM file");
-                    r = (256*data_hi + data_lo);
-
-                    if ((data_hi = file->Read_Byte ()) == EOF)
-                        throw POV_EXCEPTION(kFileDataErr, "Unexpected EOF in PPM file");
-                    if ((data_lo = file->Read_Byte ()) == EOF)
-                        throw POV_EXCEPTION(kFileDataErr, "Unexpected EOF in PPM file");
-                    g = (256*data_hi + data_lo);
-
-                    if ((data_hi = file->Read_Byte ()) == EOF)
-                        throw POV_EXCEPTION(kFileDataErr, "Unexpected EOF in PPM file");
-                    if ((data_lo = file->Read_Byte ()) == EOF)
-                        throw POV_EXCEPTION(kFileDataErr, "Unexpected EOF in PPM file");
-                    b = (256*data_hi + data_lo);
-
-                    SetEncodedRGBValue (image, x, i, gamma, 65535, r, g, b) ;
-                }
+                r = ReadNetpbmRasterValue (file, dataFormat);
+                g = ReadNetpbmRasterValue (file, dataFormat);
+                b = ReadNetpbmRasterValue (file, dataFormat);
+                SetEncodedRGBValue (image, x, i, gamma, maxImageVal, r,g,b);
             }
         }
     }
 
-    return (image) ;
+    // TODO - we may want to warn in case we haven't reached the end of file yet.
+
+    return image;
 }
 
-} // end of namespace Ppm
+} // end of namespace Netpbm
 
 }
 

source/base/image/image.cpp
@@ -54,7 +54,6 @@
 #include "base/image/iff.h"
 #include "base/image/jpeg_pov.h"
 #include "base/image/openexr.h"
-#include "base/image/pgm.h"
 #include "base/image/png_pov.h"
 #include "base/image/ppm.h"
 #include "base/image/targa.h"
@@ -3555,6 +3554,70 @@ void RGBFTMap2RGBAMap(const vector<Image::RGBFTMapEntry>& m, vector<Image::RGBAM
         n.push_back(Image::RGBAMapEntry(i->red, i->green, i->blue, RGBFTColour::FTtoA(i->filter, i->transm)));
 }
 
+Image::ImageDataType Image::GetImageDataType (ImageChannelDataType channelType, ImageChannelLayout layout)
+{
+    switch (layout)
+    {
+    case kImageChannelLayout_Gray:
+        switch (channelType)
+        {
+        case kImageChannelDataType_Int8:    return Image::Gray_Int8;
+        case kImageChannelDataType_Int16:   return Image::Gray_Int16;
+        case kImageChannelDataType_Gamma8:  return Image::Gray_Gamma8;
+        case kImageChannelDataType_Gamma16: return Image::Gray_Gamma16;
+        default:
+            POV_IMAGE_ASSERT(false);
+            break;
+        }
+        break;
+
+    case kImageChannelLayout_GrayA:
+        switch (channelType)
+        {
+        case kImageChannelDataType_Int8:    return Image::GrayA_Int8;
+        case kImageChannelDataType_Int16:   return Image::GrayA_Int16;
+        case kImageChannelDataType_Gamma8:  return Image::GrayA_Gamma8;
+        case kImageChannelDataType_Gamma16: return Image::GrayA_Gamma16;
+        default:
+            POV_IMAGE_ASSERT(false);
+            break;
+        }
+        break;
+
+    case kImageChannelLayout_RGB:
+        switch (channelType)
+        {
+        case kImageChannelDataType_Int8:    return Image::RGB_Int8;
+        case kImageChannelDataType_Int16:   return Image::RGB_Int16;
+        case kImageChannelDataType_Gamma8:  return Image::RGB_Gamma8;
+        case kImageChannelDataType_Gamma16: return Image::RGB_Gamma16;
+        default:
+            POV_IMAGE_ASSERT(false);
+            break;
+        }
+        break;
+
+    case kImageChannelLayout_RGBA:
+        switch (channelType)
+        {
+        case kImageChannelDataType_Int8:    return Image::RGBA_Int8;
+        case kImageChannelDataType_Int16:   return Image::RGBA_Int16;
+        case kImageChannelDataType_Gamma8:  return Image::RGBA_Gamma8;
+        case kImageChannelDataType_Gamma16: return Image::RGBA_Gamma16;
+        default:
+            POV_IMAGE_ASSERT(false);
+            break;
+        }
+        break;
+
+    default:
+        POV_IMAGE_ASSERT(false);
+        break;
+    }
+
+    return Image::Undefined;
+}
+
 Image *Image::Create(unsigned int w, unsigned int h, ImageDataType t, unsigned int maxRAMmbHint, unsigned int pixelsPerBlockHint)
 {
     try
@@ -3904,10 +3967,10 @@ following built-in formats: GIF, TGA, IFF, PGM, PPM, BMP.");
             return (Iff::Read(file, options));
 
         case PGM:
-            return (Pgm::Read(file, options));
+            return (Netpbm::Read(file, options));
 
         case PPM:
-            return (Ppm::Read(file, options));
+            return (Netpbm::Read(file, options));
 
         case BMP:
             return (Bmp::Read(file, options));
@@ -3997,7 +4060,7 @@ following built-in formats: TGA, PPM, BMP.");
             break;
 
         case PPM:
-            Ppm::Write(file, image, options);
+            Netpbm::Write(file, image, options);
             break;
 
         case BMP:

source/base/image/image.h
@@ -104,6 +104,22 @@ class Image
             RGBFTColourMap
         };
 
+        enum ImageChannelDataType
+        {
+            kImageChannelDataType_Int8,
+            kImageChannelDataType_Int16,
+            kImageChannelDataType_Gamma8,
+            kImageChannelDataType_Gamma16,
+        };
+
+        enum ImageChannelLayout
+        {
+            kImageChannelLayout_Gray,
+            kImageChannelLayout_GrayA,
+            kImageChannelLayout_RGB,
+            kImageChannelLayout_RGBA,
+        };
+
         enum ImageDataType
         {
             /// Value used to indicate that image decoder is free to pick the most fitting type.
@@ -197,6 +213,8 @@ class Image
         };
 
         virtual ~Image() { }
+
+        static ImageDataType GetImageDataType (ImageChannelDataType channelType, ImageChannelLayout layout);
 
         static Image *Create(unsigned int w, unsigned int h, ImageDataType t, unsigned int maxRAMmbHint, unsigned int pixelsPerBlockHint);
         static Image *Create(unsigned int w, unsigned int h, ImageDataType t, bool allowFileBacking = false);

commit c470daee693e039fadcd545c1212a8d634182dbb

Hopefully fix Unix build error introduced with commit be34de4.

source/base/image/ppm.cpp
@@ -280,7 +280,7 @@ inline static POV_UINT16 ReadNetpbmRasterValue (IStream *file, NetpbmDataFormat 
     if (lo == EOF)
         throw POV_EXCEPTION(kFileDataErr, "Unexpected end of file in Netpbm (PGM/PPM) file");
 
-    return (POV_UINT16(hi) << 8) + POV_UINT16(lo);
+    return (static_cast<POV_UINT16>(hi) << 8) + static_cast<POV_UINT16>(lo);
 }
 
 /*****************************************************************************/