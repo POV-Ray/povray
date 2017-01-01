c3ad8b20c2c7e74dafcfebdb1b16f8eb996dc968
Christoph Lipka <c-lipka@users.noreply.github.com>
2017-01-01 14:03:45 +0100

    Update copyright notices and change logs.

--- source/base/version.h
+++ source/base/version.h
@@ -12,7 +12,7 @@
 /// @parblock
 ///
 /// Persistence of Vision Ray Tracer ('POV-Ray') version 3.7.
-/// Copyright 1991-2016 Persistence of Vision Raytracer Pty. Ltd.
+/// Copyright 1991-2017 Persistence of Vision Raytracer Pty. Ltd.
 ///
 /// POV-Ray is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Affero General Public License as
@@ -57,7 +57,7 @@
 /// @{
 
 /// Copyright string.
-#define POV_RAY_COPYRIGHT "Copyright 1991-2016 Persistence of Vision Raytracer Pty. Ltd."
+#define POV_RAY_COPYRIGHT "Copyright 1991-2017 Persistence of Vision Raytracer Pty. Ltd."
 
 /// Official source code version string.
 /// @note For pre-releases this should be _equal to_ the planned final version.
