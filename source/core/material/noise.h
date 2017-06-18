POV-Ray/povray

commit cc77eea6f300091aa966060d0f166e54d0ffdea0

More refactoring of optimized noise generator mechanism and CPU feature detection.

source/core/material/noise.h
@@ -1,0 +1,203 @@
+//******************************************************************************
+///
+/// @file core/material/noise.h
+///
+/// Declarations related to noise.
+///
+/// @copyright
+/// @parblock
+///
+/// Persistence of Vision Ray Tracer ('POV-Ray') version 3.7.
+/// Copyright 1991-2017 Persistence of Vision Raytracer Pty. Ltd.
+///
+/// POV-Ray is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Affero General Public License as
+/// published by the Free Software Foundation, either version 3 of the
+/// License, or (at your option) any later version.
+///
+/// POV-Ray is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Affero General Public License for more details.
+///
+/// You should have received a copy of the GNU Affero General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///
+/// ----------------------------------------------------------------------------
+///
+/// POV-Ray is based on the popular DKB raytracer version 2.12.
+/// DKBTrace was originally written by David K. Buck.
+/// DKBTrace Ver 2.0-2.12 were written by David K. Buck & Aaron A. Collins.
+///
+/// @endparblock
+///
+//******************************************************************************
+
+#ifndef POVRAY_CORE_NOISE_H
+#define POVRAY_CORE_NOISE_H
+
+// Module config header file must be the first file included within POV-Ray unit header files
+#include "core/configcore.h"
+
+#include "core/material/warp.h"
+#include "core/math/vector.h"
+
+namespace pov
+{
+
+//##############################################################################
+///
+/// @defgroup PovCoreMaterialNoise Noise
+/// @ingroup PovCore
+///
+/// @{
+
+enum NoiseGenType
+{
+    kNoiseGen_Default        = 0, ///< Indicates that the scene's global settings noise generator should be used.
+    kNoiseGen_Original       = 1, ///< POV-Ray original noise generator (pre-v3.5).
+    kNoiseGen_RangeCorrected = 2, ///< POV-Ray original noise generator with range correction (v3.5 and later).
+    kNoiseGen_Perlin         = 3  ///< Perlin noise generator.
+};
+const int kNoiseGen_Min = 1;
+const int kNoiseGen_Max = 3;
+
+/*****************************************************************************
+* Global preprocessor defines
+******************************************************************************/
+
+// Hash1dRTableIndex assumed values in the range 0..8191
+#define Hash1dRTableIndex(a,b)   \
+    ((hashTable[(int)(a) ^ (b)] & 0xFF) * 2)
+
+// Hash2d assumed values in the range 0..8191
+#define Hash2d(a,b)   \
+    hashTable[(int)(hashTable[(int)(a)] ^ (b))]
+
+const int NOISE_MINX = -10000;
+const int NOISE_MINY = NOISE_MINX;
+const int NOISE_MINZ = NOISE_MINX;
+
+
+/*****************************************************************************
+* Global variables
+******************************************************************************/
+
+#ifdef DYNAMIC_HASHTABLE
+extern unsigned short *hashTable;
+#else
+extern ALIGN16 unsigned short hashTable[8192];
+#endif
+
+extern ALIGN16 DBL RTable[];
+
+
+/*****************************************************************************
+* Global functions
+******************************************************************************/
+
+void Initialize_Noise (void);
+void Initialize_Waves(vector<double>& waveFrequencies, vector<Vector3d>& waveSources, unsigned int numberOfWaves);
+void Free_Noise_Tables (void);
+
+DBL SolidNoise(const Vector3d& P);
+
+#ifdef TRY_OPTIMIZED_NOISE
+
+typedef DBL(*NoiseFunction) (const Vector3d& EPoint, int noise_generator);
+typedef void(*DNoiseFunction) (Vector3d& result, const Vector3d& EPoint);
+
+DBL PortableNoise(const Vector3d& EPoint, int noise_generator);
+void PortableDNoise(Vector3d& result, const Vector3d& EPoint);
+
+/// Optimized noise dispatch information.
+struct OptimizedNoiseInfo
+{
+    /// String unambiguously identifying the optimized implementation.
+    ///
+    /// @note
+    ///     The string must be _all-lowercase_ ASCII, and should contain only letters, digits,
+    ///     dashes (`-`) and/or dots (`.`).
+    ///
+    /// In general, the format should be `FEATURES-AUTHOR`[`.N`], where `FEATURES` is an
+    /// all-lowercase alphanumeric field identifying the CPU features required for the
+    /// implementation to run (e.g. `avxfma4`), `AUTHOR` is another all-lowercase alphanumeric
+    /// field identifying the person or organization who provided the implementation,
+    /// and `N` is an optional integer numeric field identifying a particular revision of the
+    /// implementation. For compiler-optimized code based on the portable implementation, the
+    /// `AUTHOR` field should be `generic`.
+    ///
+    const char* name;
+
+    /// String providing noteworthy details about the implementation.
+    const char* info;
+
+    /// Pointer to the optimized implementation of @ref PortableNoise().
+    NoiseFunction noise;
+
+    /// Pointer to the optimized implementation of @ref PortableDNoise().
+    DNoiseFunction dNoise;
+
+    /// Pointer to a function testing whether the optimized implementation is supported.
+    /// A value of `NULL` indicates universal support.
+    bool(*supported)();
+
+    /// Pointer to a function testing for additional auto-selection constraints.
+    /// A value of `NULL` indicates the absence of such constraints.
+    bool(*recommended)();
+
+    /// Pointer to the initialization function.
+    /// A value of `NULL` indicates that initialization is not required.
+    void(*init)();
+};
+
+/// Optimized noise dispatch table.
+///
+/// This table contains a list of all available optimized noise generator implementations, sorted
+/// by descending order of preference. The end of the table is indicated by an entry with the
+/// `name` field set to `NULL`.
+///
+/// @note
+///     The non-optimized portable default noise generator implementation should _not_ be included
+///     in the list.
+///
+/// @note
+///     This table must be implemented by platform-specific code.
+///
+extern OptimizedNoiseInfo gaOptimizedNoiseInfo[];
+
+/// Get the recommended noise generator implementation for the current runtime environment.
+const OptimizedNoiseInfo* GetRecommendedOptimizedNoise();
+
+/// Get a specific noise generator implementation.
+const OptimizedNoiseInfo* GetOptimizedNoise(std::string name);
+
+extern NoiseFunction Noise;
+extern DNoiseFunction DNoise;
+
+void Initialise_NoiseDispatch();
+
+#else // TRY_OPTIMIZED_NOISE
+
+class PortableNoiseGenerator
+{
+public:
+    static DBL Noise(const Vector3d& EPoint, int noise_generator);
+    static void DNoise(Vector3d& result, const Vector3d& EPoint);
+};
+
+inline DBL Noise(const Vector3d& EPoint, int noise_generator) { return PortableNoise(EPoint, noise_generator); }
+inline void DNoise(Vector3d& result, const Vector3d& EPoint) { PortableDNoise(result, EPoint); }
+
+#endif // TRY_OPTIMIZED_NOISE
+
+DBL Turbulence (const Vector3d& EPoint, const GenericTurbulenceWarp* Turb, int noise_generator);
+void DTurbulence (Vector3d& result, const Vector3d& EPoint, const GenericTurbulenceWarp* Turb);
+
+/// @}
+///
+//##############################################################################
+
+}
+
+#endif // POVRAY_CORE_NOISE_H

platform/x86/avx/avxnoise.cpp
@@ -48,8 +48,7 @@
 #include MACHINE_INTRINSICS_H
 #endif
 
-#include "core/material/pattern.h"
-#include "core/material/texture.h"
+#include "core/material/noise.h"
 
 /// @file
 /// @attention
@@ -318,7 +317,6 @@ DBL AVXNoise(const Vector3d& EPoint, int noise_generator)
 
 #if CHECK_FUNCTIONAL
     {
-        extern DBL PortableNoise(const Vector3d& EPoint, int noise_generator);
         DBL orig_sum = PortableNoise(EPoint, noise_generator);
         if (fabs(orig_sum - sum) >= EPSILON)
         {
@@ -492,7 +490,6 @@ void AVXDNoise(Vector3d& result, const Vector3d& EPoint)
 
 #if CHECK_FUNCTIONAL
     {
-        extern void PortableDNoise(Vector3d& result, const Vector3d& EPoint);
         Vector3d portable_res;
         PortableDNoise(portable_res , param);
         if (fabs(portable_res[X] - result[X]) >= EPSILON)

platform/x86/avx2fma3/avx2fma3noise.cpp
@@ -48,8 +48,7 @@
 #include MACHINE_INTRINSICS_H
 #endif
 
-#include "core/material/pattern.h"
-#include "core/material/texture.h"
+#include "core/material/noise.h"
 
 /// @file
 /// @attention
@@ -305,7 +304,6 @@ DBL AVX2FMA3Noise(const Vector3d& EPoint, int noise_generator)
 
 #if CHECK_FUNCTIONAL
     {
-        extern DBL PortableNoise(const Vector3d& EPoint, int noise_generator);
         DBL orig_sum = PortableNoise(EPoint, noise_generator);
         if (fabs(orig_sum - sum) >= EPSILON)
         {
@@ -479,7 +477,6 @@ void AVX2FMA3DNoise(Vector3d& result, const Vector3d& EPoint)
 
 #if CHECK_FUNCTIONAL
     {
-        extern void PortableDNoise(Vector3d& result, const Vector3d& EPoint);
         Vector3d portable_res;
         PortableDNoise(portable_res , param);
         if (fabs(portable_res[X] - result[X]) >= EPSILON)

platform/x86/avxfma4/avxfma4noise.cpp
@@ -43,8 +43,7 @@
 #include MACHINE_INTRINSICS_H
 #endif
 
-#include "core/material/pattern.h"
-#include "core/material/texture.h"
+#include "core/material/noise.h"
 
 /// @file
 /// @attention

platform/x86/cpuid.cpp
@@ -109,6 +109,7 @@ static unsigned long long getXCR0()
 
 // Masks for relevant CPUID result bits.
 #define CPUID_00000001_ECX_FMA3_MASK    (0x1 << 12)
+#define CPUID_00000001_ECX_XSAVE_MASK   (0x1 << 26)
 #define CPUID_00000001_ECX_OSXSAVE_MASK (0x1 << 27)
 #define CPUID_00000001_ECX_AVX_MASK     (0x1 << 28)
 #define CPUID_00000001_EDX_SSE2_MASK    (0x1 << 26)
@@ -133,99 +134,264 @@ static bool OSSavesAVXRegisters()
     return ((xcrFeatureMask & (XCR0_SSE_MASK|XCR0_AVX_MASK)) == (XCR0_SSE_MASK|XCR0_AVX_MASK));
 }
 
-bool HaveSSE2()
+enum CPUVendorId
 {
-    int info[4];
-    // Verify the relevant CPUID leaves are supported
-    CPUID(info, 0x0);
-    if (info[CPUID_EAX] < 0x1)
-        return false;
-    // Read the relevant CPUID leaves
-    CPUID(info, 0x1);
-    bool osxsave = ((info[CPUID_ECX] & CPUID_00000001_ECX_OSXSAVE_MASK) != 0);
-    bool sse2    = ((info[CPUID_EDX] & CPUID_00000001_EDX_SSE2_MASK)    != 0);
-    // Aggregate the information
-    return osxsave && sse2 && OSSavesSSERegisters();
-}
+    kCPUVendor_Unrecognized = 0,
+    kCPUVendor_AMD,
+    kCPUVendor_Intel,
+    kCPUVendor_VM,
+};
 
-bool HaveAVX()
+struct CPUVendorInfo
 {
-    int info[4];
-    // Verify the relevant CPUID leaves are supported
-    CPUID(info, 0x0);
-    if (info[CPUID_EAX] < 0x1)
-        return false;
-    // Evaluate the relevant CPUID leaves
-    CPUID(info, 0x1);
-    bool osxsave = ((info[CPUID_ECX] & CPUID_00000001_ECX_OSXSAVE_MASK) != 0);
-    bool avx     = ((info[CPUID_ECX] & CPUID_00000001_ECX_AVX_MASK)     != 0);
-    // Aggregate the information
-    return osxsave && avx && OSSavesAVXRegisters();
-}
+    CPUVendorId id;
+    const char* cpuidString;
+};
 
-bool HaveAVX2()
-{
-    int info[4];
-    // Verify the relevant CPUID leaves are supported
-    CPUID(info, 0x0);
-    if (info[CPUID_EAX] < 0x7)
-        return false;
-    // Evaluate the relevant CPUID leaves
-    CPUID(info, 0x1);
-    bool osxsave = ((info[CPUID_ECX] & CPUID_00000001_ECX_OSXSAVE_MASK) != 0);
-    bool avx     = ((info[CPUID_ECX] & CPUID_00000001_ECX_AVX_MASK)     != 0);
-    CPUID(info, 0x7);
-    bool avx2    = ((info[CPUID_EBX] & CPUID_00000007_EBX_AVX2_MASK)    != 0);
-    // Aggregate the information
-    return osxsave && avx && avx2 && OSSavesAVXRegisters();
-}
+CPUVendorInfo gCPUVendorInfo[] = {
+    // Common CPU manufacturers.
+    { kCPUVendor_AMD,           "AuthenticAMD" },
+    { kCPUVendor_Intel,         "GenuineIntel" },
+    // Known virtual machines.
+    { kCPUVendor_VM,            "Microsoft Hv" },   // Microsoft Hyper-V or Windows Virtual PC
+    { kCPUVendor_VM,            " lrpepyh vr"  },   // Parallels
+    { kCPUVendor_VM,            "KVMKVMKVM"    },   // KVM
+    { kCPUVendor_VM,            "VMwareVMware" },   // VMWare
+    { kCPUVendor_VM,            "XenVMMXenVMM" },   // Xen HVM
+    // End of list.
+    { kCPUVendor_Unrecognized,  NULL }
+};
 
-bool HaveAVXFMA4()
+struct CPUIDInfo
 {
-    int info[4];
-    // Verify the relevant CPUID leaves are supported
-    CPUID(info, 0x0);
-    if (info[CPUID_EAX] < 0x1)
-        return false;
-    CPUID(info, 0x80000000);
-    if (info[CPUID_EAX] < (int)0x80000001)
-        return false;
-    // Evaluate the relevant CPUID leaves
-    CPUID(info, 0x1);
-    bool osxsave = ((info[CPUID_ECX] & CPUID_00000001_ECX_OSXSAVE_MASK) != 0);
-    bool avx     = ((info[CPUID_ECX] & CPUID_00000001_ECX_AVX_MASK)     != 0);
-    CPUID(info, 0x80000001);
-    bool fma4    = ((info[CPUID_ECX] & CPUID_80000001_ECX_FMA4_MASK)    != 0);
-    // Aggregate the information
-    return osxsave && avx && fma4 && OSSavesAVXRegisters();
-}
+    CPUVendorId vendorId;
+    bool        xsave  : 1;
+    bool        osxsave: 1;
+    bool        sse2   : 1;
+    bool        avx    : 1;
+    bool        avx2   : 1;
+    bool        fma3   : 1;
+    bool        fma4   : 1;
+#if POV_CPUINFO_DEBUG
+    char        vendor[13];
+#endif
+    CPUIDInfo();
+};
 
-bool HaveAVX2FMA3()
+CPUIDInfo::CPUIDInfo() :
+    xsave(false),
+    osxsave(false),
+    sse2(false),
+    avx(false),
+    avx2(false),
+    fma3(false),
+    fma4(false),
+    vendorId(kCPUVendor_Unrecognized)
 {
     int info[4];
-    // Verify the relevant CPUID leaves are supported
     CPUID(info, 0x0);
-    if (info[CPUID_EAX] < 0x7)
-        return false;
-    // Evaluate the relevant CPUID leaves
-    CPUID(info, 0x1);
-    bool osxsave = ((info[CPUID_ECX] & CPUID_00000001_ECX_OSXSAVE_MASK) != 0);
-    bool avx     = ((info[CPUID_ECX] & CPUID_00000001_ECX_AVX_MASK)     != 0);
-    bool fma3    = ((info[CPUID_ECX] & CPUID_00000001_ECX_FMA3_MASK)    != 0);
-    CPUID(info, 0x7);
-    bool avx2    = ((info[CPUID_EBX] & CPUID_00000007_EBX_AVX2_MASK)    != 0);
-    // Aggregate the information
-    return osxsave && avx && fma3 && avx2 && OSSavesAVXRegisters();
-}
-
-bool IsIntelCPU()
-{
-    int info[4];
-    char vendor[12];
-    CPUID(info, 0x0);
-    std::memcpy(vendor, &info[CPUID_EBX], 4);
+#if !POV_CPUINFO_DEBUG
+    char vendor[13];
+#endif
+    std::memcpy(vendor,     &info[CPUID_EBX], 4);
     std::memcpy(vendor + 4, &info[CPUID_EDX], 4);
     std::memcpy(vendor + 8, &info[CPUID_ECX], 4);
-
-    return std::strncmp("GenuineIntel", vendor, 12) == 0;
+    vendor[12] = '\0';
+    for (CPUVendorInfo* p = gCPUVendorInfo; p->cpuidString != NULL; ++p)
+    {
+        if (strcmp(vendor, p->cpuidString) == 0)
+        {
+            vendorId = p->id;
+            break;
+        }
+    }
+    int maxLeaf = info[CPUID_EAX];
+    if (maxLeaf >= 0x1)
+    {
+        CPUID(info, 0x1);
+        fma3    = ((info[CPUID_ECX] & CPUID_00000001_ECX_FMA3_MASK)    != 0);
+        xsave   = ((info[CPUID_ECX] & CPUID_00000001_ECX_XSAVE_MASK)   != 0);
+        osxsave = ((info[CPUID_ECX] & CPUID_00000001_ECX_OSXSAVE_MASK) != 0);
+        avx     = ((info[CPUID_ECX] & CPUID_00000001_ECX_AVX_MASK)     != 0);
+        sse2    = ((info[CPUID_EDX] & CPUID_00000001_EDX_SSE2_MASK)    != 0);
+    }
+    if (maxLeaf >= 0x7)
+    {
+        CPUID(info, 0x7);
+        avx2    = ((info[CPUID_EBX] & CPUID_00000007_EBX_AVX2_MASK)    != 0);
+    }
+    CPUID(info, 0x80000000);
+    int maxLeafExt = info[CPUID_EAX];
+    if (maxLeafExt >= (int)0x80000001)
+    {
+        CPUID(info, 0x80000001);
+        fma4    = ((info[CPUID_ECX] & CPUID_80000001_ECX_FMA4_MASK)    != 0);
+    }
 }
+
+struct OSInfo
+{
+    bool xcr0_sse : 1;
+    bool xcr0_avx : 1;
+    OSInfo(const CPUIDInfo& cpuinfo);
+};
+
+OSInfo::OSInfo(const CPUIDInfo& cpuinfo) :
+    xcr0_sse(false),
+    xcr0_avx(false)
+{
+    if (cpuinfo.xsave && cpuinfo.osxsave)
+    {
+        unsigned long long xcrFeatureMask = GET_XCR0();
+        xcr0_sse = ((xcrFeatureMask & XCR0_SSE_MASK) != 0);
+        xcr0_avx = ((xcrFeatureMask & XCR0_AVX_MASK) != 0);
+    }
+}
+
+/// @warning
+///     _Do not re-order the members of this structure!_
+///     @par
+///     The members of this data structure must be initialized in a particular order,
+///     which according to the C++ standard (and contrary to naive expectations)
+///     does _not_ depend on the order of member-initializers in the constructor,
+///     but rather on the _order of declaration_.
+///
+struct CPUInfo::Data
+{
+    CPUIDInfo   cpuidInfo;
+    OSInfo      osInfo;
+    Data() : cpuidInfo(), osInfo(cpuidInfo) {}
+};
+
+bool CPUInfo::SupportsSSE2()
+{
+    return gpData->cpuidInfo.osxsave
+        && gpData->cpuidInfo.sse2
+        && gpData->osInfo.xcr0_sse;
+}
+
+bool CPUInfo::SupportsAVX()
+{
+    return gpData->cpuidInfo.osxsave
+        && gpData->cpuidInfo.avx
+        && gpData->osInfo.xcr0_sse
+        && gpData->osInfo.xcr0_avx;
+}
+
+bool CPUInfo::SupportsAVX2()
+{
+    return gpData->cpuidInfo.osxsave
+        && gpData->cpuidInfo.avx
+        && gpData->cpuidInfo.avx2
+        && gpData->osInfo.xcr0_sse
+        && gpData->osInfo.xcr0_avx;
+}
+
+bool CPUInfo::SupportsFMA3()
+{
+    return gpData->cpuidInfo.fma3;
+}
+
+bool CPUInfo::SupportsFMA4()
+{
+    return gpData->cpuidInfo.fma4;
+}
+
+bool CPUInfo::IsIntel()
+{
+    return gpData->cpuidInfo.vendorId == kCPUVendor_Intel;
+}
+
+bool CPUInfo::IsAMD()
+{
+    return gpData->cpuidInfo.vendorId == kCPUVendor_AMD;
+}
+
+bool CPUInfo::IsVM()
+{
+    return gpData->cpuidInfo.vendorId == kCPUVendor_VM;
+}
+
+std::string CPUInfo::GetFeatures()
+{
+    std::vector<const char*> features;
+
+    if (IsAMD())
+        features.push_back("AMD");
+    else if (IsIntel())
+        features.push_back("Intel");
+    else if (IsVM())
+        features.push_back("VM");
+
+    if (SupportsSSE2())
+        features.push_back("SSE2");
+    if (SupportsAVX())
+        features.push_back("AVX");
+    if (SupportsAVX2())
+        features.push_back("AVX2");
+    if (SupportsFMA3())
+        features.push_back("FMA3");
+    if (SupportsFMA4())
+        features.push_back("FMA4");
+
+    std::string result;
+    for (std::vector<const char*>::const_iterator i = features.begin(); i != features.end(); ++i)
+    {
+        if (!result.empty())
+            result.append(",");
+        result.append(*i);
+    }
+
+    return result;
+}
+
+#if POV_CPUINFO_DEBUG
+std::string CPUInfo::GetDetails()
+{
+    std::vector<std::string> cpuidFeatures;
+
+    std::string vendor = gpData->cpuidInfo.vendor;
+    cpuidFeatures.push_back("'" + vendor + "'");
+    if (gpData->cpuidInfo.avx)
+        cpuidFeatures.push_back("AVX");
+    if (gpData->cpuidInfo.avx2)
+        cpuidFeatures.push_back("AVX2");
+    if (gpData->cpuidInfo.fma3)
+        cpuidFeatures.push_back("FMA");
+    if (gpData->cpuidInfo.fma4)
+        cpuidFeatures.push_back("FMA4");
+    if (gpData->cpuidInfo.osxsave)
+        cpuidFeatures.push_back("OSXSAVE");
+    if (gpData->cpuidInfo.sse2)
+        cpuidFeatures.push_back("SSE2");
+    if (gpData->cpuidInfo.xsave)
+        cpuidFeatures.push_back("XSAVE");
+
+    std::vector<std::string> xcr0Features;
+
+    if (gpData->osInfo.xcr0_avx)
+        xcr0Features.push_back("AVX");
+    if (gpData->osInfo.xcr0_sse)
+        xcr0Features.push_back("SSE");
+
+    std::string cpuidResult;
+    for (std::vector<std::string>::const_iterator i = cpuidFeatures.begin(); i != cpuidFeatures.end(); ++i)
+    {
+        if (!cpuidResult.empty())
+            cpuidResult.append(",");
+        cpuidResult.append(*i);
+    }
+
+    std::string xcr0Result;
+    for (std::vector<std::string>::const_iterator i = xcr0Features.begin(); i != xcr0Features.end(); ++i)
+    {
+        if (!xcr0Result.empty())
+            xcr0Result.append(",");
+        xcr0Result.append(*i);
+    }
+
+    return "CPUID:" + cpuidResult + ";XCR0:" + xcr0Result;
+}
+#endif
+
+const CPUInfo::Data* CPUInfo::gpData(new CPUInfo::Data);

platform/x86/cpuid.h
@@ -39,22 +39,24 @@
 
 #include "base/configbase.h"
 
-/// Test whether SSE2 is supported by both CPU and OS.
-bool HaveSSE2();
-
-/// Test whether AVX is supported by both CPU and OS.
-bool HaveAVX();
-
-/// Test whether AVX2 is supported by both CPU and OS.
-bool HaveAVX2();
-
-/// Test whether AVX and FMA4 are supported by both CPU and OS.
-bool HaveAVXFMA4();
-
-/// Test whether AVX2 and FMA3 are supported by both CPU and OS.
-bool HaveAVX2FMA3();
-
-/// Test whether CPU is an intel product
-bool IsIntelCPU();
+class CPUInfo
+{
+public:
+    static bool SupportsSSE2();             ///< Test whether CPU and OS support SSE2.
+    static bool SupportsAVX();              ///< Test whether CPU and OS support AVX.
+    static bool SupportsAVX2();             ///< Test whether CPU and OS support AVX2.
+    static bool SupportsFMA3();             ///< Test whether CPU and OS support FMA3.
+    static bool SupportsFMA4();             ///< Test whether CPU and OS support FMA4.
+    static bool IsIntel();                  ///< Test whether CPU is genuine Intel product.
+    static bool IsAMD();                    ///< Test whether CPU is genuine AMD product.
+    static bool IsVM();                     ///< Test whether CPU cannot be detected reliably due to running in a VM.
+    static std::string GetFeatures();       ///< Query ASCII text string summarizing the features detected.
+#if POV_CPUINFO_DEBUG
+    static std::string GetDetails();        ///< Query ASCII text string detailing the raw CPUID (and related) information gathered.
+#endif
+private:
+    struct Data;
+    static const Data* gpData;
+};
 
 #endif // POVRAY_CPUID_H

platform/x86/optimizednoise.cpp
@@ -37,6 +37,8 @@
 // Unit header file must be the first file included within POV-Ray *.cpp files (pulls in config)
 #include "optimizednoise.h"
 
+#include "core/material/noise.h"
+
 #ifdef TRY_OPTIMIZED_NOISE_AVX2FMA3
 #include "avx2fma3/avx2fma3noise.h"
 #endif
@@ -60,54 +62,63 @@
 namespace pov
 {
 
-bool TryOptimizedNoise(NoiseFunction* pFnNoise, DNoiseFunction* pFnDNoise,
-                       std::string* pImpl, std::string* pInfo)
-{
-    bool doInit = (pFnNoise || pFnDNoise);
+static bool AVXSupported()      { return CPUInfo::SupportsAVX(); }
+static bool AVXFMA4Supported()  { return CPUInfo::SupportsAVX() && CPUInfo::SupportsFMA4(); }
+static bool AVX2FMA3Supported() { return CPUInfo::SupportsAVX2() && CPUInfo::SupportsFMA3(); }
+
+/// List of optimized noise implementations.
+///
+/// @note
+///     Entries must be listed in descending order of preference.
+///
+OptimizedNoiseInfo gaOptimizedNoiseInfo[] = {
 #ifdef TRY_OPTIMIZED_NOISE_AVX2FMA3
-    if (HaveAVX2FMA3() && IsIntelCPU())
     {
-        if (doInit)     AVX2FMA3NoiseInit();
-        if (pFnNoise)   *pFnNoise   = AVX2FMA3Noise;
-        if (pFnDNoise)  *pFnDNoise  = AVX2FMA3DNoise;
-        if (pImpl)      *pImpl      = "AVX2FMA3-Intel";
-        if (pInfo)      *pInfo      = "hand-optimized by Intel";
-        return true;
-    }
+        "avx2fma3-intel",           // name,
+        "hand-optimized by Intel",  // info,
+        AVX2FMA3Noise,              // noise,
+        AVX2FMA3DNoise,             // dNoise,
+        AVX2FMA3Supported,          // supported,
+        CPUInfo::IsIntel,           // recommended,
+        AVX2FMA3NoiseInit           // init
+    },
 #endif
 #ifdef TRY_OPTIMIZED_NOISE_AVXFMA4
-    if (HaveAVXFMA4())
     {
-        if (pFnNoise)   *pFnNoise   = AVXFMA4Noise;
-        if (pFnDNoise)  *pFnDNoise  = AVXFMA4DNoise;
-        if (pImpl)      *pImpl      = "AVXFMA4-AMD.2";
-        if (pInfo)      *pInfo      = "hand-optimized by AMD, 2017-04 update";
-        return true;
-    }
+        "avxfma4-amd.2",            // name,
+        "hand-optimized by AMD, 2017-04 update", // info,
+        AVXFMA4Noise,               // noise,
+        AVXFMA4DNoise,              // dNoise,
+        AVXFMA4Supported,           // supported,
+        NULL,                       // recommended,
+        NULL                        // init
+    },
 #endif
 #ifdef TRY_OPTIMIZED_NOISE_AVX
-    if (HaveAVX() && IsIntelCPU())
     {
-        if (doInit)     AVXNoiseInit();
-        if (pFnNoise)   *pFnNoise   = AVXNoise;
-        if (pFnDNoise)  *pFnDNoise  = AVXDNoise;
-        if (pImpl)      *pImpl      = "AVX-Intel";
-        if (pInfo)      *pInfo      = "hand-optimized by Intel";
-        return true;
-    }
+        "avx-intel",                // name,
+        "hand-optimized by Intel",  // info,
+        AVXNoise,                   // noise,
+        AVXDNoise,                  // dNoise,
+        AVXSupported,               // supported,
+        CPUInfo::IsIntel,           // recommended,
+        AVXNoiseInit                // init
+    },
 #endif
 #ifdef TRY_OPTIMIZED_NOISE_AVX_PORTABLE
-    if (HaveAVX())
     {
-        if (pFnNoise)   *pFnNoise   = AVXPortableNoise;
-        if (pFnDNoise)  *pFnDNoise  = AVXPortableDNoise;
-        if (pImpl)      *pImpl      = "AVX-Portable";
-        if (pInfo)      *pInfo      = "auto-optimized by compiler";
-        return true;
-    }
+        "avx-generic",              // name,
+        "compiler-optimized",       // info,
+        AVXPortableNoise,           // noise,
+        AVXPortableDNoise,          // dNoise,
+        AVXSupported,               // supported,
+        NULL,                       // recommended,
+        NULL                        // init
+    },
 #endif
-    return false;
-}
+    // End-of-list entry.
+    { NULL }
+};
 
 }
 

platform/x86/optimizednoise.h
@@ -38,6 +38,5 @@
 #define POVRAY_OPTIMIZEDNOISE_H
 
 #include "core/configcore.h"
-#include "core/material/texture.h"
 
 #endif // POVRAY_OPTIMIZEDNOISE_H

commit 3bc30fbd316aa3fa296b2109835b43a214a3f33e

Fix bug introduced with commit cc77eea that broke builds without optimized noise.

source/core/material/noise.h
@@ -102,13 +102,13 @@ void Free_Noise_Tables (void);
 
 DBL SolidNoise(const Vector3d& P);
 
+DBL PortableNoise(const Vector3d& EPoint, int noise_generator);
+void PortableDNoise(Vector3d& result, const Vector3d& EPoint);
+
 #ifdef TRY_OPTIMIZED_NOISE
 
 typedef DBL(*NoiseFunction) (const Vector3d& EPoint, int noise_generator);
 typedef void(*DNoiseFunction) (Vector3d& result, const Vector3d& EPoint);
-
-DBL PortableNoise(const Vector3d& EPoint, int noise_generator);
-void PortableDNoise(Vector3d& result, const Vector3d& EPoint);
 
 /// Optimized noise dispatch information.
 struct OptimizedNoiseInfo
@@ -178,13 +178,6 @@ extern DNoiseFunction DNoise;
 void Initialise_NoiseDispatch();
 
 #else // TRY_OPTIMIZED_NOISE
-
-class PortableNoiseGenerator
-{
-public:
-    static DBL Noise(const Vector3d& EPoint, int noise_generator);
-    static void DNoise(Vector3d& result, const Vector3d& EPoint);
-};
 
 inline DBL Noise(const Vector3d& EPoint, int noise_generator) { return PortableNoise(EPoint, noise_generator); }
 inline void DNoise(Vector3d& result, const Vector3d& EPoint) { PortableDNoise(result, EPoint); }