d7d663ae0b5cdf8f22821c45d3838f28632bda53
eblanca <elioblanca@yahoo.it>
2017-07-11 19:43:46 +0200

    simplify constructor

--- source/base/textstreambuffer.cpp
+++ source/base/textstreambuffer.cpp
@@ -55,20 +55,24 @@
 // this must be the last file included
 #include "base/povdebug.h"
 
+unsigned int GetTerminalWidth()
+{
+#if defined(TIOCGWINSZ) && defined(HAVE_IOCTL)
+    struct winsize w;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == 0)
+        return (unsigned int)w.ws_col;
+#endif
+    return 80;
+}
+
 namespace pov_base
 {
 
 TextStreamBuffer::TextStreamBuffer(size_t buffersize, unsigned int wrapwidth)
 {
-#if defined(TIOCGWINSZ) && defined(HAVE_IOCTL)
-    struct winsize w;
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == 0)
-        wrap = (unsigned int)w.ws_col;
-    else
-#endif
-    wrap = wrapwidth;
     boffset = 0;
     bsize = buffersize;
+    wrap = ~0;
     curline = 0;
     buffer = new char[bsize];
     if(buffer == NULL)
@@ -89,6 +93,9 @@ TextStreamBuffer::~TextStreamBuffer()
 void TextStreamBuffer::printf(const char *format, ...)
 {
     va_list marker;
+
+    if (wrap == ~0)
+        wrap = GetTerminalWidth();
 
     va_start(marker, format);
     vsnprintf(&buffer[boffset], bsize - boffset - 1, format, marker);
