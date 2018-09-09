POV-Ray/povray

commit 1d120c9009fd989a8db898061a9b8bb3fd541163

Silence some clang warnings.

source/base/image/colourspace.cpp
@@ -8,7 +8,7 @@
 /// @parblock
 ///
 /// Persistence of Vision Ray Tracer ('POV-Ray') version 3.8.
-/// Copyright 1991-2017 Persistence of Vision Raytracer Pty. Ltd.
+/// Copyright 1991-2018 Persistence of Vision Raytracer Pty. Ltd.
 ///
 /// POV-Ray is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Affero General Public License as
@@ -433,16 +433,18 @@ bool TranscodingGammaCurve::Matches(const GammaCurvePtr& p) const
 
 SimpleGammaCurvePtr GetGammaCurve(GammaTypeId type, float param)
 {
+    SimpleGammaCurvePtr gamma;
     switch (type)
     {
-        case kPOVList_GammaType_Neutral:    return NeutralGammaCurve::Get();
-        case kPOVList_GammaType_PowerLaw:   return PowerLawGammaCurve::GetByDecodingGamma(param);
-        case kPOVList_GammaType_SRGB:       return SRGBGammaCurve::Get();
-        case kPOVList_GammaType_BT709:      return BT709GammaCurve::Get();
-        case kPOVList_GammaType_BT1361:     return BT1361GammaCurve::Get();
-        case kPOVList_GammaType_BT2020:     return BT2020GammaCurve::Get();
-        default:                            POV_ASSERT (false);
+        case kPOVList_GammaType_Neutral:    gamma = NeutralGammaCurve::Get();
+        case kPOVList_GammaType_PowerLaw:   gamma = PowerLawGammaCurve::GetByDecodingGamma(param);
+        case kPOVList_GammaType_SRGB:       gamma = SRGBGammaCurve::Get();
+        case kPOVList_GammaType_BT709:      gamma = BT709GammaCurve::Get();
+        case kPOVList_GammaType_BT1361:     gamma = BT1361GammaCurve::Get();
+        case kPOVList_GammaType_BT2020:     gamma = BT2020GammaCurve::Get();
+        default:                            POV_COLOURSPACE_ASSERT (false);
     }
+    return gamma;
 }
 
 }

source/base/colour.cpp
@@ -8,7 +8,7 @@
 /// @parblock
 ///
 /// Persistence of Vision Ray Tracer ('POV-Ray') version 3.8.
-/// Copyright 1991-2017 Persistence of Vision Raytracer Pty. Ltd.
+/// Copyright 1991-2018 Persistence of Vision Raytracer Pty. Ltd.
 ///
 /// POV-Ray is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Affero General Public License as
@@ -42,10 +42,13 @@
 namespace pov_base
 {
 
+template class GenericColour<ColourChannel>;
+template class GenericColour<PreciseColourChannel>;
+
 #if (NUM_COLOUR_CHANNELS == 3)
 
     // Approximate dominant wavelengths of primary hues.
-    // Source: 3D Computer Graphics by John Vince (Addison Wesely)
+    // Source: 3D Computer Graphics by John Vince (Addison Wesley)
     // These are user-adjustable with the irid_wavelength keyword.
     // Red = 700 nm  Grn = 520 nm Blu = 480 nm
     // Divided by 1000 gives: rwl = 0.70;  gwl = 0.52;  bwl = 0.48;

source/base/colour.h
@@ -9,7 +9,7 @@
 /// @parblock
 ///
 /// Persistence of Vision Ray Tracer ('POV-Ray') version 3.8.
-/// Copyright 1991-2017 Persistence of Vision Raytracer Pty. Ltd.
+/// Copyright 1991-2018 Persistence of Vision Raytracer Pty. Ltd.
 ///
 /// POV-Ray is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Affero General Public License as
@@ -1787,6 +1787,9 @@ inline GenericColour<T> Sqrt(const GenericColour<T>& a) { return a.Sqrt(); }
 /// @relates GenericColour
 template<typename T>
 inline GenericColour<T> Cos(const GenericColour<T>& a) { return a.Cos(); }
+
+extern template class GenericColour<ColourChannel>;
+extern template class GenericColour<PreciseColourChannel>;
 
 typedef GenericColour<ColourChannel>         MathColour;        ///< Standard precision colour.
 typedef GenericColour<PreciseColourChannel>  PreciseMathColour; ///< High precision colour.

source/frontend/shelloutprocessing.cpp
@@ -8,7 +8,7 @@
 /// @parblock
 ///
 /// Persistence of Vision Ray Tracer ('POV-Ray') version 3.8.
-/// Copyright 1991-2017 Persistence of Vision Raytracer Pty. Ltd.
+/// Copyright 1991-2018 Persistence of Vision Raytracer Pty. Ltd.
 ///
 /// POV-Ray is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Affero General Public License as
@@ -439,6 +439,10 @@ bool ShelloutProcessing::PostProcessEvent(void)
                         cancelParameters = sh->Parameters();
                         cancelOutput = LastShelloutResult();
                         return true;
+
+                    default:
+                        // Do nothing special.
+                        break;
                 }
             }
 
@@ -464,6 +468,10 @@ bool ShelloutProcessing::PostProcessEvent(void)
                                 skipCallouts = true;
 
                             return true;
+
+                        default:
+                            // Do nothing special.
+                            break;
                     }
                     break;
 
@@ -495,6 +503,10 @@ bool ShelloutProcessing::PostProcessEvent(void)
                                 skipReason = str(boost::format("skip frame %1%") % (frameNo + 1));
                             }
                             break;
+
+                        default:
+                            // Do nothing special.
+                            break;
                     }
                     break;
 
@@ -510,6 +522,10 @@ bool ShelloutProcessing::PostProcessEvent(void)
                             cancelParameters = skipParameters = sh->Parameters();
                             cancelOutput = skipOutput = LastShelloutResult();
                             cancelReason = skipReason = "skip all remaining frames";
+                            break;
+
+                        default:
+                            // Do nothing special.
                             break;
                     }
                     break;

source/frontend/simplefrontend.h
@@ -8,7 +8,7 @@
 /// @parblock
 ///
 /// Persistence of Vision Ray Tracer ('POV-Ray') version 3.8.
-/// Copyright 1991-2017 Persistence of Vision Raytracer Pty. Ltd.
+/// Copyright 1991-2018 Persistence of Vision Raytracer Pty. Ltd.
 ///
 /// POV-Ray is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Affero General Public License as
@@ -365,9 +365,12 @@ State SimpleFrontend<PARSER_MH, FILE_MH, RENDER_MH, IMAGE_MH>::Process()
                     state = kRendering;
 
                     return kRendering;
-            }
 
-            return kParsing;
+                default:
+                    return state;
+            }
+            POV_FRONTEND_ASSERT(false); // All cases of the preceding switch should return.
+
         case kRendering:
             switch(renderFrontend.GetViewState(viewId))
             {
@@ -408,9 +411,12 @@ State SimpleFrontend<PARSER_MH, FILE_MH, RENDER_MH, IMAGE_MH>::Process()
                         state = kDone;
                         return kDone;
                     }
-            }
 
-            return kRendering;
+                default:
+                    return state;
+            }
+            POV_FRONTEND_ASSERT(false); // All cases of the preceding switch should return.
+
         case kStopping:
             if(renderFrontend.GetSceneState(sceneId) == SceneData::Scene_Ready || renderFrontend.GetSceneState(sceneId) == SceneData::Scene_Failed)
             {
@@ -433,9 +439,11 @@ State SimpleFrontend<PARSER_MH, FILE_MH, RENDER_MH, IMAGE_MH>::Process()
             state = kReady;
 
             return kReady;
-    }
 
-    return state;
+        default:
+            return state;
+    }
+    POV_FRONTEND_ASSERT(false); // All cases of the preceding switch should return.
 }
 
 template<class PARSER_MH, class FILE_MH, class RENDER_MH, class IMAGE_MH>

source/povms/povmscpp.cpp
@@ -8,7 +8,7 @@
 /// @parblock
 ///
 /// Persistence of Vision Ray Tracer ('POV-Ray') version 3.8.
-/// Copyright 1991-2017 Persistence of Vision Raytracer Pty. Ltd.
+/// Copyright 1991-2018 Persistence of Vision Raytracer Pty. Ltd.
 ///
 /// POV-Ray is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Affero General Public License as
@@ -100,7 +100,7 @@ POVMS_Container::POVMS_Container()
     data.ptr = NULL;
 }
 
-POVMS_Container::~POVMS_Container()
+POVMS_Container::~POVMS_Container() noexcept(false)
 {
     // do nothing for now
 }
@@ -333,7 +333,7 @@ POVMS_Attribute::POVMS_Attribute(const POVMS_Attribute& source)
         throw POV_EXCEPTION_CODE(err);
 }
 
-POVMS_Attribute::~POVMS_Attribute()
+POVMS_Attribute::~POVMS_Attribute() noexcept(false)
 {
     int err;
 
@@ -599,7 +599,7 @@ POVMS_List::POVMS_List(const POVMS_List& source)
         throw POV_EXCEPTION_CODE(err);
 }
 
-POVMS_List::~POVMS_List()
+POVMS_List::~POVMS_List() noexcept(false)
 {
     int err;
 
@@ -822,7 +822,7 @@ POVMS_Object::POVMS_Object(const POVMS_Object& source)
     }
 }
 
-POVMS_Object::~POVMS_Object()
+POVMS_Object::~POVMS_Object() noexcept(false)
 {
     int err;
 

source/povms/povmscpp.h
@@ -9,7 +9,7 @@
 /// @parblock
 ///
 /// Persistence of Vision Ray Tracer ('POV-Ray') version 3.8.
-/// Copyright 1991-2017 Persistence of Vision Raytracer Pty. Ltd.
+/// Copyright 1991-2018 Persistence of Vision Raytracer Pty. Ltd.
 ///
 /// POV-Ray is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Affero General Public License as
@@ -67,7 +67,7 @@ class POVMS_Container
         friend class POVMS_MessageReceiver;
     public:
         POVMS_Container();
-        virtual ~POVMS_Container();
+        virtual ~POVMS_Container() noexcept(false);
 
         POVMSType Type() const;
         size_t Size() const;
@@ -95,7 +95,7 @@ class POVMS_Attribute : public POVMS_Container
         POVMS_Attribute(std::vector<POVMSType>& value);
         POVMS_Attribute(POVMSAttribute& convert);
         POVMS_Attribute(const POVMS_Attribute& source);
-        virtual ~POVMS_Attribute();
+        virtual ~POVMS_Attribute() noexcept(false);
 
         POVMS_Attribute& operator=(const POVMS_Attribute& source);
 
@@ -128,7 +128,7 @@ class POVMS_List : public POVMS_Container
         POVMS_List();
         POVMS_List(POVMSAttributeList& convert);
         POVMS_List(const POVMS_List& source);
-        virtual ~POVMS_List();
+        virtual ~POVMS_List() noexcept(false);
 
         POVMS_List& operator=(const POVMS_List& source);
 
@@ -196,7 +196,7 @@ class POVMS_Object : public POVMS_Container
         POVMS_Object(POVMSObject& convert);
         POVMS_Object(POVMSObjectPtr convert);
         POVMS_Object(const POVMS_Object& source);
-        ~POVMS_Object();
+        ~POVMS_Object() noexcept(false);
 
         POVMS_Object& operator=(const POVMS_Object& source);
 

vfe/unix/unixconsole.cpp
@@ -8,7 +8,7 @@
 /// @parblock
 ///
 /// Persistence of Vision Ray Tracer ('POV-Ray') version 3.8.
-/// Copyright 1991-2017 Persistence of Vision Raytracer Pty. Ltd.
+/// Copyright 1991-2018 Persistence of Vision Raytracer Pty. Ltd.
 ///
 /// POV-Ray is free software: you can redistribute it and/or modify
 /// it under the terms of the GNU Affero General Public License as
@@ -231,6 +231,9 @@ static void PrintStatusChanged (vfeSession *session, State force = kUnknown)
 #else
             fprintf (stderr, "==== [Paused...] ===========================================================\n");
 #endif
+            break;
+        default:
+            // Do nothing special.
             break;
     }
 }

commit 63580949b437db9627276f95d3e882fe34eefffa

Fix bug introduced with commit 1d120c90.

source/base/image/colourspace.cpp
@@ -436,13 +436,13 @@ SimpleGammaCurvePtr GetGammaCurve(GammaTypeId type, float param)
     SimpleGammaCurvePtr gamma;
     switch (type)
     {
-        case kPOVList_GammaType_Neutral:    gamma = NeutralGammaCurve::Get();
-        case kPOVList_GammaType_PowerLaw:   gamma = PowerLawGammaCurve::GetByDecodingGamma(param);
-        case kPOVList_GammaType_SRGB:       gamma = SRGBGammaCurve::Get();
-        case kPOVList_GammaType_BT709:      gamma = BT709GammaCurve::Get();
-        case kPOVList_GammaType_BT1361:     gamma = BT1361GammaCurve::Get();
-        case kPOVList_GammaType_BT2020:     gamma = BT2020GammaCurve::Get();
-        default:                            POV_COLOURSPACE_ASSERT (false);
+        case kPOVList_GammaType_Neutral:    gamma = NeutralGammaCurve::Get();                       break;
+        case kPOVList_GammaType_PowerLaw:   gamma = PowerLawGammaCurve::GetByDecodingGamma(param);  break;
+        case kPOVList_GammaType_SRGB:       gamma = SRGBGammaCurve::Get();                          break;
+        case kPOVList_GammaType_BT709:      gamma = BT709GammaCurve::Get();                         break;
+        case kPOVList_GammaType_BT1361:     gamma = BT1361GammaCurve::Get();                        break;
+        case kPOVList_GammaType_BT2020:     gamma = BT2020GammaCurve::Get();                        break;
+        default:                            POV_COLOURSPACE_ASSERT(false);                          break;
     }
     return gamma;
 }