POV-Ray/povray

commit 177ec827c57251375e1b92059cce5324558a75a5

[parser] Add error handling to font-related code.

source/parser/parser.cpp
@@ -6583,82 +6583,98 @@ ObjectPtr Parser::Parse_TrueType ()
                     EXIT
                 END_CASE
             END_EXPECT
-            font = mFontResolver.GetFont(fontName, style);
+            try
+            {
+                font = mFontResolver.GetFont(fontName, style);
+                if (font == nullptr)
+                    Error("Failed to load system font '%s'.", UCS2toASCIIString(fontName));
+            }
+            catch (FontProcessingException& e)
+            {
+                Error("Failed to load system font '%s' (%s).", UCS2toASCIIString(fontName), e.what());
+            }
         END_CASE
         OTHERWISE
             Expectation_Error ("ttf or internal");
         END_CASE
     END_EXPECT
 
-    // TODO - Keep FontEngine object around to improve performance.
-    FontEnginePtr fontEngine = std::make_shared<FontEngine>();
-
-    // TODO - Keep FontFace objects around to improve performance.
-    FontFacePtr fontFace = std::make_shared<FontFace>(fontEngine, font);
-
-    cmap = FontEngine::kAnyCMAP;
-    charset = CharsetID::kUndefined;
-    EXPECT_ONE
-        CASE(CMAP_TOKEN)
-            Warning("Text primitive 'cmap' extension is experimental and may be "
-                    "subject to future changes.");
-            Parse_Begin();
-            cmap =  POV_UINT16(Parse_Float()) << 16;
-            Parse_Comma();
-            cmap += POV_UINT16(Parse_Float());
-            charset = CharsetID::kUCS4;
-            EXPECT_ONE
-                CASE(CHARSET_TOKEN)
-                    charset = CharsetID(POV_UINT16(Parse_Float()));
-                    if (Charset::Get(charset) == nullptr)
-                        Error("Unknown character set %i", int(charset));
-                END_CASE
-                OTHERWISE
-                    UNGET
-                END_CASE
-            END_EXPECT
-            Parse_End();
-        END_CASE
-        OTHERWISE
-            UNGET
-        END_CASE
-    END_EXPECT
-
-    /*** Object = Create_TTF(); */
-    Parse_Comma();
-
-    /* Parse the text string to be rendered */
-    text_string = Parse_String();
-    Parse_Comma();
-
-    /* Get the extrusion depth */
-    depth = Parse_Float(); Parse_Comma ();
-
-    /* Get the offset vector */
-    Parse_Vector(offset);
-
-    if (fabs(offset.z()) > EPSILON)
-        Warning("Text primitive offset in Z dimension ignored.");
-
-    auto shapedGlyphs = fontFace->GetShapedGlyphs(text_string, Vector2d(offset.x(), offset.y()));
-
     std::vector<Prism*> prisms;
 
-    for (auto& glyph : shapedGlyphs)
+    try
     {
-        auto controlPoints = fontFace->GetCubicBezierOutline(glyph);
+        // TODO - Keep FontEngine object around to improve performance.
+        FontEnginePtr fontEngine = std::make_shared<FontEngine>();
 
-        for (size_t i = 0; i < controlPoints.size() / 2; ++i)
-            std::swap(controlPoints[i], controlPoints[controlPoints.size() - i - 1]);
+        // TODO - Keep FontFace objects around to improve performance.
+        FontFacePtr fontFace = std::make_shared<FontFace>(fontEngine, font);
 
-        Prism* prism = new Prism();
-        prism->Spline_Type = BEZIER_SPLINE;
-        prism->Height1 = -depth;
-        prism->Height2 = 0;
-        prism->Number = controlPoints.size();
-        prism->Compute_Prism(controlPoints.data(), GetParserDataPtr());
-        prism->Compute_BBox();
-        prisms.push_back(prism);
+        cmap = FontEngine::kAnyCMAP;
+        charset = CharsetID::kUndefined;
+        EXPECT_ONE
+            CASE(CMAP_TOKEN)
+                Warning("Text primitive 'cmap' extension is experimental and may be "
+                        "subject to future changes.");
+                Parse_Begin();
+                cmap =  POV_UINT16(Parse_Float()) << 16;
+                Parse_Comma();
+                cmap += POV_UINT16(Parse_Float());
+                charset = CharsetID::kUCS4;
+                EXPECT_ONE
+                    CASE(CHARSET_TOKEN)
+                        charset = CharsetID(POV_UINT16(Parse_Float()));
+                        if (Charset::Get(charset) == nullptr)
+                            Error("Unknown character set %i", int(charset));
+                    END_CASE
+                    OTHERWISE
+                        UNGET
+                    END_CASE
+                END_EXPECT
+                Parse_End();
+            END_CASE
+            OTHERWISE
+                UNGET
+            END_CASE
+        END_EXPECT
+
+        /*** Object = Create_TTF(); */
+        Parse_Comma();
+
+        /* Parse the text string to be rendered */
+        text_string = Parse_String();
+        Parse_Comma();
+
+        /* Get the extrusion depth */
+        depth = Parse_Float(); Parse_Comma ();
+
+        /* Get the offset vector */
+        Parse_Vector(offset);
+
+        if (fabs(offset.z()) > EPSILON)
+            Warning("Text primitive offset in Z dimension ignored.");
+
+        auto shapedGlyphs = fontFace->GetShapedGlyphs(text_string, Vector2d(offset.x(), offset.y()));
+
+        for (auto& glyph : shapedGlyphs)
+        {
+            auto controlPoints = fontFace->GetCubicBezierOutline(glyph);
+
+            for (size_t i = 0; i < controlPoints.size() / 2; ++i)
+                std::swap(controlPoints[i], controlPoints[controlPoints.size() - i - 1]);
+
+            Prism* prism = new Prism();
+            prism->Spline_Type = BEZIER_SPLINE;
+            prism->Height1 = -depth;
+            prism->Height2 = 0;
+            prism->Number = controlPoints.size();
+            prism->Compute_Prism(controlPoints.data(), GetParserDataPtr());
+            prism->Compute_BBox();
+            prisms.push_back(prism);
+        }
+    }
+    catch (FontProcessingException& e)
+    {
+        Error("Failed to create text object (%s).", e.what());
     }
 
     if (prisms.size() == 1)

platform/windows/osfontresolver.cpp
@@ -66,49 +66,51 @@ FontReferencePtr WindowsFontResolver::GetFont(const UCS2String& name, FontStyle 
         // Too long to be a valid system font name.
         return nullptr;
 
-    char* buffer = nullptr;
+    FontReferencePtr result = nullptr;
 
     HDC hdc = CreateCompatibleDC(nullptr);
-    if (hdc == nullptr)
-        ; // TODO throw tantrum
-
-    bool bold   = ((style & FontStyle::kBold)   != FontStyle::kRegular);
-    bool italic = ((style & FontStyle::kItalic) != FontStyle::kRegular);
-    int  weight = (bold ? 700 : 400);
-
-    HFONT fontHandle = CreateFontW(
-        0,                      // cHeight
-        0,                      // cWidth
-        0,                      // cEscapement
-        0,                      // cOrientation
-        (int)weight,            // cWeight
-        (DWORD)italic,          // bItalic
-        FALSE,                  // bUnderline
-        FALSE,                  // bStrikeOut
-        DEFAULT_CHARSET,        // iCharSet
-        OUT_DEFAULT_PRECIS,     // iOutPrecision
-        CLIP_DEFAULT_PRECIS,    // iClipPrecision
-        DEFAULT_QUALITY,        // iQuality
-        DEFAULT_PITCH | FF_DONTCARE, // iPitchAndFamily
-        (LPCWSTR)name.c_str()   // pszFaceName
-    );
-
-    SelectObject(hdc, fontHandle);
-    auto size = ::GetFontData(hdc, 0, 0, nullptr, 0);
-
-    if (size == 0)
-        return nullptr;
-
-    buffer = new char[size];
-    if (GetFontData(hdc, 0, 0, buffer, size) != size)
+    if (hdc != nullptr)
     {
-        delete[] buffer;
-        return nullptr;
+        bool bold   = ((style & FontStyle::kBold)   != FontStyle::kRegular);
+        bool italic = ((style & FontStyle::kItalic) != FontStyle::kRegular);
+        int  weight = (bold ? 700 : 400);
+
+        HFONT fontHandle = CreateFontW(
+            0,                      // cHeight
+            0,                      // cWidth
+            0,                      // cEscapement
+            0,                      // cOrientation
+            (int)weight,            // cWeight
+            (DWORD)italic,          // bItalic
+            FALSE,                  // bUnderline
+            FALSE,                  // bStrikeOut
+            DEFAULT_CHARSET,        // iCharSet
+            OUT_DEFAULT_PRECIS,     // iOutPrecision
+            CLIP_DEFAULT_PRECIS,    // iClipPrecision
+            DEFAULT_QUALITY,        // iQuality
+            DEFAULT_PITCH | FF_DONTCARE, // iPitchAndFamily
+            (LPCWSTR)name.c_str()   // pszFaceName
+        );
+
+        if (fontHandle != nullptr)
+        {
+            SelectObject(hdc, fontHandle);
+            auto size = ::GetFontData(hdc, 0, 0, nullptr, 0);
+            if (size != 0)
+            {
+                auto buffer = new char[size];
+                if (GetFontData(hdc, 0, 0, buffer, size) == size)
+                    result = std::make_shared<BufferedFontFile>(buffer, size, BufferedFontFile::Mode::kTransferOwnership);
+                else
+                    delete[] buffer;
+            }
+            DeleteObject(fontHandle);
+        }
+
+        DeleteDC(hdc);
     }
 
-    DeleteDC(hdc);
-
-    return std::make_shared<BufferedFontFile>(buffer, size, BufferedFontFile::Mode::kTransferOwnership);
+    return result;
 }
 
 FontResolver& OSGetFontResolver()

source/parser/font.cpp
@@ -61,6 +61,23 @@ namespace pov_parser
 
 //******************************************************************************
 
+FontProcessingException::FontProcessingException(FT_Error ftErr) :
+    mFreeTypeError(ftErr)
+{}
+
+const char* FontProcessingException::what() const
+{
+    #undef FTERRORS_H_
+    #define FT_ERROR_START_LIST     switch(mFreeTypeError) {
+    #define FT_ERRORDEF(e,v,s)          case v: return s;
+    #define FT_ERROR_END_LIST       };
+    #include FT_ERRORS_H
+
+    return "(unknown error code)";
+}
+
+//******************************************************************************
+
 FontReference::~FontReference()
 {}
 
@@ -108,7 +125,7 @@ FontEngine::FontEngine() :
     if (error != FT_Err_Ok)
     {
         mFTLibrary = nullptr;
-        // TODO throw tantrum
+        throw FontProcessingException(error);
     }
 }
 
@@ -128,13 +145,13 @@ FontFace::FontFace(const FontEnginePtr& pEngine, const FontReferencePtr& pFontRe
     if (error != FT_Err_Ok)
     {
         mFTFace = nullptr;
-        // TODO throw tantrum
+        throw FontProcessingException(error);
     }
     else
     {
         error = FT_Set_Char_Size(mFTFace, mFTFace->units_per_EM, mFTFace->units_per_EM, 0, 0);
         if (error != FT_Err_Ok)
-            ; // TODO throw tantrum
+            throw FontProcessingException(error);
         mScalingFactor = 1.0 / mFTFace->units_per_EM;
     }
 }
@@ -152,7 +169,7 @@ Vector2d FontFace::GetKerning(const GlyphIndex g1, const GlyphIndex g2)
         FT_Vector kerningOffset;
         FT_Error error = FT_Get_Kerning(mFTFace, g1, g2, FT_KERNING_UNFITTED, &kerningOffset);
         if (error != FT_Err_Ok)
-            ; // TODO throw tantrum
+            throw FontProcessingException(error);
         return Vector2d(kerningOffset.x * mScalingFactor, 0);
     }
     else
@@ -163,7 +180,7 @@ Vector2d FontFace::GetAdvance(const GlyphIndex g)
 {
     FT_Error error = FT_Load_Glyph(mFTFace, g, FT_LOAD_NO_BITMAP | FT_LOAD_NO_HINTING | FT_LOAD_NO_RECURSE);
     if (error != FT_Err_Ok)
-        ; // TODO throw tantrum
+        throw FontProcessingException(error);
     return Vector2d(mFTFace->glyph->linearHoriAdvance * mScalingFactor / 1024.0, 0);
 }
 
@@ -321,9 +338,9 @@ OutlineControlPoints FontFace::GetCubicBezierOutline(const ShapedGlyph& glyph)
 
     FT_Error error = FT_Load_Glyph(mFTFace, glyph.glyphIndex, FT_LOAD_NO_BITMAP | FT_LOAD_NO_HINTING);
     if (error != FT_Err_Ok)
-        ; // TODO throw tantrum
+        throw FontProcessingException(error);
     if (mFTFace->glyph->format != FT_GLYPH_FORMAT_OUTLINE)
-        ; // TODO throw tantrum
+        throw FontProcessingException(FT_Err_Invalid_Outline); // TODO Maybe use a dedicated exception.
     FT_Outline& outline = mFTFace->glyph->outline;
 
     FT_Outline_Funcs ftFuncs;
@@ -333,9 +350,10 @@ OutlineControlPoints FontFace::GetCubicBezierOutline(const ShapedGlyph& glyph)
     ftFuncs.cubic_to = CubicTo;
     ftFuncs.shift = 0;
     ftFuncs.delta = 0;
+
     error = FT_Outline_Decompose(&outline, &ftFuncs, &outlineProcessor);
     if (error != FT_Err_Ok)
-        ; // TODO throw tantrum
+        throw FontProcessingException(error);
 
     return controlPoints;
 }

source/parser/font.h
@@ -72,6 +72,16 @@ using namespace pov_base;
 
 //------------------------------------------------------------------------------
 
+struct FontProcessingException : std::exception
+{
+    FontProcessingException(FT_Error ftErr);
+    virtual const char* what() const override;
+protected:
+    FT_Error mFreeTypeError;
+};
+
+//------------------------------------------------------------------------------
+
 class FontReference
 {
 public:

commit 30308ed15cfa1b6718272f87697f0a7047aaf2c4

[parser] Fix another Unix build error introduced with commit 177ec827.

source/parser/parser.cpp
@@ -6587,11 +6587,11 @@ ObjectPtr Parser::Parse_TrueType ()
             {
                 font = mFontResolver.GetFont(fontName, style);
                 if (font == nullptr)
-                    Error("Failed to load system font '%s'.", UCS2toASCIIString(fontName));
+                    Error("Failed to load system font '%s'.", UCS2toASCIIString(fontName).c_str());
             }
             catch (FontProcessingException& e)
             {
-                Error("Failed to load system font '%s' (%s).", UCS2toASCIIString(fontName), e.what());
+                Error("Failed to load system font '%s' (%s).", UCS2toASCIIString(fontName).c_str(), e.what());
             }
         END_CASE
         OTHERWISE